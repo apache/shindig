b40de0abb2e0288888d5b14b59e01f4b8a8f3da0
Zhen Wang <zhen@apache.org>
2008-01-11 04:49:52 +0000

    Added skeletons for the "dynamic-height" and "settitle" features.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@611045 13f79535-47bb-0310-9956-ffa450edef68

---
+++ features/dynamic-height/dynamic-height.js
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+/**
+ * @fileoverview This library augments gadgets.Window with functionality
+ * to change the height of a gadget dynamically.
+ */
+
+gadgets.Window = gadgets.Window || {};
+
+/**
+ * Adjusts the gadget height
+ * @param {Number} opt_height Preferred height in pixels.
+ */
+gadgets.Window.adjustHeight = function(opt_height) {
+  // TODO
+};
+
