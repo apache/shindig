caaf507b463df3363cc35e21366c17c98a869e2d
Kevin Brown <etnu@apache.org>
2008-01-24 00:30:28 +0000

    Implemented https://issues.apache.org/jira/browse/SHINDIG-33 and marked Executor-only ctor as deprecated.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@614753 13f79535-47bb-0310-9956-ffa450edef68

--- java/gadgets/src/test/java/org/apache/shindig/gadgets/GadgetServerTest.java
+++ java/gadgets/src/test/java/org/apache/shindig/gadgets/GadgetServerTest.java
@@ -1,19 +1,19 @@
-/*
- * $Id$
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
- * Copyright 2007 The Apache Software Foundation
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.apache.shindig.gadgets;
 
@@ -32,7 +32,7 @@ import java.util.concurrent.Executors;
 
 public class GadgetServerTest extends EasyMockTestCase {
   final Executor executor = Executors.newCachedThreadPool();
-  final GadgetServer gadgetServer = new GadgetServer(executor);
+  final GadgetServer gadgetServer;
   final RemoteContentFetcher fetcher = mock(RemoteContentFetcher.class);
   @SuppressWarnings(value="unchecked")
   final GadgetDataCache<GadgetSpec> specCache = mock(GadgetDataCache.class);
@@ -40,11 +40,13 @@ public class GadgetServerTest extends EasyMockTestCase {
   final GadgetDataCache<MessageBundle> bundleCache = mock(GadgetDataCache.class);
 
   public GadgetServerTest() throws GadgetException {
-    GadgetFeatureRegistry registry = new GadgetFeatureRegistry(null);
-    gadgetServer.setGadgetFeatureRegistry(registry);
-    gadgetServer.setContentFetcher(fetcher);
-    gadgetServer.setSpecCache(specCache);
-    gadgetServer.setMessageBundleCache(bundleCache);
+    // TODO: need to test for configuration errors.
+    gadgetServer = new GadgetServer(new GadgetServerConfig()
+        .setExecutor(executor)
+        .setSpecCache(specCache)
+        .setMessageBundleCache(bundleCache)
+        .setFeatureRegistry(new GadgetFeatureRegistry(null))
+        .setContentFetcher(fetcher));
   }
 
   public void testGadgetSpecNotInCache() throws Exception {
