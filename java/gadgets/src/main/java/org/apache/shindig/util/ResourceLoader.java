f2196cbe1525dbffb9b3951444b0eedf344c8f91
Kevin Brown <etnu@apache.org>
2008-02-20 10:19:19 +0000

    Eased restrictions on SyndicatorConfig & JsFeatureLoader to accept any file with an appropriate extension rather than requiring specific names. In practice this simplifies the code greatly and the removed code paths were never used anyway.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@629406 13f79535-47bb-0310-9956-ffa450edef68

--- java/gadgets/src/main/java/org/apache/shindig/util/ResourceLoader.java
+++ java/gadgets/src/main/java/org/apache/shindig/util/ResourceLoader.java
@@ -24,18 +24,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.JarURLConnection;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 import java.util.logging.Logger;
 
 /**
@@ -62,58 +50,6 @@ public class ResourceLoader {
     }
 
     return InputStreamConsumer.readToString(is);
-  }
-
-  /**
-   * Loads resources recursively and returns the contents of all matching
-   * files.
-   *
-   * @param paths The base paths to look for the desired files.
-   * @param file The name of the files to actually return content for.
-   * @return A map of file paths to their contents.
-   *
-   * @throws IOException
-   */
-  public static Map<String, String> getContent(String[] paths, String file)
-      throws IOException {
-    ClassLoader cl = ResourceLoader.class.getClassLoader();
-    Map<String, String> out = new HashMap<String, String>();
-
-    for (String path : paths) {
-      logger.info("Looking for " + file + " in " + path);
-      Enumeration<URL> mappedResources = cl.getResources(path);
-      while (mappedResources.hasMoreElements()) {
-        URL resourceUrl =  mappedResources.nextElement();
-        if ("file".equals(resourceUrl.getProtocol())) {
-          try {
-            File f = new File(resourceUrl.toURI());
-            out.put(path, getContent(f));
-          } catch (URISyntaxException e) {
-            logger.warning("Unable to load file " + resourceUrl.toString());
-          }
-        } else {
-          URLConnection urlConnection = resourceUrl.openConnection();
-          List<String> fullPaths = new LinkedList<String>();
-          if (urlConnection instanceof JarURLConnection) {
-            JarURLConnection jarUrlConn = (JarURLConnection)urlConnection;
-            JarFile jar = jarUrlConn.getJarFile();
-
-            Enumeration<JarEntry> jarEntries = jar.entries();
-            while (jarEntries.hasMoreElements()) {
-              JarEntry jarEntry =  jarEntries.nextElement();
-              if (jarEntry.getName().startsWith(path) &&
-                  jarEntry.getName().endsWith(file)) {
-                fullPaths.add(jarEntry.getName());
-              }
-            }
-          }
-          for (String res : fullPaths) {
-            out.put(res, getContent(res));
-          }
-        }
-      }
-    }
-    return Collections.unmodifiableMap(out);
   }
 
   /**
