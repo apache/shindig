911022908159c88443b1a6f1661dfe1d130685d2
Cassandra Doll <doll@apache.org>
2008-02-22 02:50:31 +0000

    The first part of a very very simple implementation of a sever side backed opensocial container. This involves creating the opensocial-0.7 feature with a json based container which talks back to a servlet at /socialdata.
    This code is littered with todos and needs tons of fleshing out. However, it does successfully render the SocialHelloWorld gadget and gets us started down the path of supporting opensocial for real. 
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@630072 13f79535-47bb-0310-9956-ffa450edef68

---
+++ features/opensocial-0.7/jsoncontainer.js
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+/**
+ * @fileoverview Json based opensocial container.
+ */
+
+
+JsonContainer = function() {
+  opensocial.Container.call(this);
+};
+JsonContainer.inherits(opensocial.Container);
+
+JsonContainer.prototype.getEnvironment = function() {
+  return this.environment_;
+};
+
+JsonContainer.prototype.createJson = function(requestObjects) {
+  var jsonObjects = [];
+  for (var i = 0; i < requestObjects.length; i++) {
+    jsonObjects[i] = requestObjects[i].request.jsonParams;
+  }
+  return gadgets.json.stringify(jsonObjects);
+};
+
+JsonContainer.prototype.requestData = function(dataRequest, callback) {
+  callback = callback || {};
+
+  var requestObjects = dataRequest.getRequestObjects();
+  if (requestObjects.length == 0) {
+    callback(new opensocial.DataResponse({}, true));
+    return;
+  }
+
+  var jsonText = this.createJson(requestObjects);
+
+  var sendResponse = function(result) {
+    result = result.data;
+
+    if (!result || result['globalError']) {
+      callback(new opensocial.DataResponse({}, true));
+      return;
+    }
+
+    var responses = result['responses'] || [];
+    var globalError = false;
+
+    var responseMap = {};
+    for (var i = 0; i < responses.length; i++) {
+      var response = responses[i];
+      var rawData = response['response'];
+
+      var processedData = requestObjects[i].request.processResponse(rawData);
+      globalError = globalError || processedData.hadError();
+      responseMap[requestObjects[i].key] = processedData;
+    }
+
+    var dataResponse = new opensocial.DataResponse(responseMap, globalError);
+    callback(dataResponse);
+  };
+
+  new BatchRequest(jsonText, sendResponse).send();
+};
+
+JsonContainer.prototype.newFetchPersonRequest = function(id,
+    opt_params) {
+  var me = this;
+  var peopleRequest = this.newFetchPeopleRequest(id, opt_params);
+  peopleRequest.processResponse = function(rawJson) {
+    return new opensocial.ResponseItem(null,
+        me.createPersonFromJson(rawJson[0]));
+  };
+  return peopleRequest;
+};
+
+JsonContainer.prototype.newFetchPeopleRequest = function(idSpec,
+    opt_params) {
+  var me = this;
+  return new RequestItem(
+      {'type' : 'FETCH_PEOPLE', 'idSpec' : idSpec, 'params': opt_params},
+      function(rawJson) {
+        var people = [];
+        for (var i = 0; i < rawJson.length; i++) {
+          people.push(me.createPersonFromJson(rawJson[i]));
+          // TODO: isOwner, isViewer
+        }
+        return new opensocial.ResponseItem(null, // TODO: Original request
+            new opensocial.Collection(people));
+      });
+};
+
+JsonContainer.prototype.createPersonFromJson = function(serverJson) {
+  // TODO: Probably move this into a subclass of person and make it cover
+  // all fields
+  var name = new opensocial.Name(serverJson["name"]);
+  return new opensocial.Person({"id" : serverJson["id"], "name" : name});
+}
+
+JsonContainer.prototype.newFetchPersonAppDataRequest = function(
+    idSpec, keys) {
+  return new RequestItem({'type' : 'FETCH_PERSON_APP_DATA', 'idSpec' : idSpec,
+    'keys' : keys});
+};
+
+JsonContainer.prototype.newUpdatePersonAppDataRequest = function(
+    id, key, value) {
+  return new RequestItem({'type' : 'UPDATE_PERSON_APP_DATA', 'id' : id,
+    'key' : key, 'value' : value});
+};
+
+JsonContainer.prototype.newFetchActivitiesRequest = function(
+    idSpec, opt_params) {
+  return new RequestItem({'type' : 'FETCH_ACTIVITIES', 'idSpec' : idSpec},
+      function(rawJson) {
+        var activities = [];
+        for (var i = 0; i < rawJson.length; i++) {
+          activities.push(new opensocial.Activity(rawJson[i]));
+        }
+        return new opensocial.ResponseItem(null, // TODO: Original request
+            {'activities' : new opensocial.Collection(activities)});
+      });
+};
+
+RequestItem = function(jsonParams, processResponse) {
+  this.jsonParams = jsonParams;
+  this.processResponse = processResponse ||
+    function (rawJson) {
+      return new opensocial.ResponseItem(null, rawJson); // TODO: Original request
+    };
+};
---
+++ java/gadgets/src/main/java/org/apache/shindig/social/http/SocialDataServlet.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.apache.shindig.social.http;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet for serving social data. This is a very basic hardcoded inital file.
+ * This will expand to be more sophisticated as time goes on.
+ */
+public class SocialDataServlet extends HttpServlet {
+  private static final Logger logger
+      = Logger.getLogger("org.apache.shindig.social");
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    // TODO: Get the security token
+    // TODO: Allow saving data
+    // TODO: Don't use string concatentation for json output
+    // TODO: etc, etc, etc 
+
+    String requestParam = req.getParameter("request");
+    try {
+      JSONArray requestItems = new JSONArray(requestParam);
+      String jsonResponse = "{'responses' : [";
+      int length = requestItems.length();
+
+      for (int i = 0; i < length; i++) {
+        String jsonData = "";
+
+        JSONObject requestItem = requestItems.getJSONObject(i);
+        String type = requestItem.getString("type");
+
+        if (type.equals("FETCH_PEOPLE")) {
+          String idSpec = requestItem.getString("idSpec");
+          if (idSpec.equals("VIEWER") || idSpec.equals("OWNER")) {
+            jsonData = "[{'id' : 'john.doe', 'name' : {'unstructured' : 'John Doe'}}]";
+          } else if (idSpec.equals("VIEWER_FRIENDS")) {
+            jsonData = "[{'id' : 'jane.doe', 'name' : {'unstructured' : 'Jane Doe'}}, " +
+                "{'id' : 'george.doe', 'name' : {'unstructured' : 'George Doe'}}]";
+          }
+
+        } else if (type.equals("FETCH_PERSON_APP_DATA")) {
+          String idSpec = requestItem.getString("idSpec");
+          if (idSpec.equals("VIEWER")) {
+            jsonData = "{'john.doe' : {'count' : 3}}";
+          } else if (idSpec.equals("VIEWER_FRIENDS")) {
+            jsonData = "{'jane.doe' : {'count' : 7}, " +
+                "'george.doe' : {'count' : 2}}";
+          }
+        }
+
+        jsonResponse += "{'response' : " + jsonData + "}";
+        if (i < length - 1) {
+          jsonResponse += ",";
+        }
+      }
+      jsonResponse += "]}";
+      PrintWriter writer = resp.getWriter();
+      writer.write(jsonResponse);
+
+    } catch (JSONException e) {
+      // TODO: handle the exception here
+    }
+
+  }
+}
