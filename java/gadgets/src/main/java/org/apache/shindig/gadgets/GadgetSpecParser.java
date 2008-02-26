bf5ba5b685ba4ec58dfb236322289a4f180a54e8
Kevin Brown <etnu@apache.org>
2008-02-26 01:33:16 +0000

    Modified spec parser to treat unknown user pref data types as STRING so that they can still be modified.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@631058 13f79535-47bb-0310-9956-ffa450edef68

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/GadgetSpecParser.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/GadgetSpecParser.java
@@ -35,7 +35,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -540,7 +539,7 @@ public class GadgetSpecParser {
             return dt;
           }
         }
-        return null;
+        return UserPref.DataType.STRING;
       }
 
       public String getName() {
