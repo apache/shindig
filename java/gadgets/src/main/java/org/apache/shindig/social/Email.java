b07d18e84c43758a8a1da9412187c7659b4433d5
Cassandra Doll <doll@apache.org>
2008-03-11 13:47:35 +0000

    Patch from Vincent Demay - SHINDIG-116
    This added more java pojos corresponding to the opensocial objects. Also cleans up the related test to use JSONObjects instead of string comparison. 
    
    I had to move the test from AbstractSocialDataTest to SocialDataTest because it seems that it wasn't being run when the classname started with "Abstract".
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@635923 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/gadgets/src/main/java/org/apache/shindig/social/Email.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.apache.shindig.social;
+
+/**
+ * see
+ * http://code.google.com/apis/opensocial/docs/0.7/reference/opensocial.Email.Field.html
+ *
+ */
+public class Email extends AbstractSocialData {
+  private String address;
+  private String type;
+
+  public Email(String address, String type) {
+    this.address = address;
+    this.type = type;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+}
