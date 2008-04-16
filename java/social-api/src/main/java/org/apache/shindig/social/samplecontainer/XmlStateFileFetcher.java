f5a21fa9dea0b204b68c67e4532374a049061b7d
Cassandra Doll <doll@apache.org>
2008-04-16 11:16:12 +0000

    Patch from SHINDIG-199 Moved the social java classes from java/gadgets to java/social-api. Did some clumsy maven stuff to make this still compile.
    The sample container is broken, future patches will fix it. 
    
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@648665 13f79535-47bb-0310-9956-ffa450edef68

--- java/social-api/src/main/java/org/apache/shindig/social/samplecontainer/XmlStateFileFetcher.java
+++ java/social-api/src/main/java/org/apache/shindig/social/samplecontainer/XmlStateFileFetcher.java
@@ -11,7 +11,6 @@ import org.apache.shindig.social.opensocial.model.MediaItem;
 import org.apache.shindig.social.opensocial.model.Name;
 import org.apache.shindig.social.opensocial.model.Person;
 import org.apache.shindig.social.opensocial.model.Phone;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -20,6 +19,8 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URI;
@@ -28,9 +29,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * @author Cassandra Doll <doll@google.com>
