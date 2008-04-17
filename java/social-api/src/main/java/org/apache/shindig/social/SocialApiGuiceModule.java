27f24926276157e63e8e45d36b4cbdf3f1cfef91
Cassandra Doll <doll@apache.org>
2008-04-17 13:19:42 +0000

    Removed the gadgets guice dependency on the social directories by creating a new social specific guice module.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@649085 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/social-api/src/main/java/org/apache/shindig/social/SocialApiGuiceModule.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.shindig.social;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.TypeLiteral;
+import org.apache.shindig.social.opensocial.ActivitiesService;
+import org.apache.shindig.social.opensocial.PeopleService;
+import org.apache.shindig.social.opensocial.DataService;
+import org.apache.shindig.social.opensocial.OpenSocialDataHandler;
+import org.apache.shindig.social.samplecontainer.BasicActivitiesService;
+import org.apache.shindig.social.samplecontainer.BasicPeopleService;
+import org.apache.shindig.social.samplecontainer.BasicDataService;
+import org.apache.shindig.social.samplecontainer.StateFileDataHandler;
+import org.apache.shindig.gadgets.RemoteContentFetcher;
+import org.apache.shindig.gadgets.BasicRemoteContentFetcher;
+import org.apache.shindig.gadgets.GadgetTokenDecoder;
+import org.apache.shindig.gadgets.BasicGadgetTokenDecoder;
+
+import java.util.Properties;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Provides http component injection on top of existing components.
+ */
+public class SocialApiGuiceModule extends AbstractModule {
+
+  /** {@inheritDoc} */
+  @Override
+  protected void configure() {
+    bind(RemoteContentFetcher.class).to(BasicRemoteContentFetcher.class);
+    bind(GadgetTokenDecoder.class).to(BasicGadgetTokenDecoder.class);
+
+    bind(PeopleService.class).to(BasicPeopleService.class);
+    bind(DataService.class).to(BasicDataService.class);
+    bind(ActivitiesService.class).to(BasicActivitiesService.class);
+
+    bind(new TypeLiteral<List<GadgetDataHandler>>() {})
+        .toProvider(GadgetDataHandlersProvider.class);
+  }
+
+  public static class GadgetDataHandlersProvider
+      implements Provider<List<GadgetDataHandler>> {
+    List<GadgetDataHandler> handlers;
+
+    @Inject
+    public GadgetDataHandlersProvider(OpenSocialDataHandler
+        openSocialDataHandler, StateFileDataHandler stateFileHandler) {
+      handlers = new ArrayList<GadgetDataHandler>();
+      handlers.add(openSocialDataHandler);
+      handlers.add(stateFileHandler);
+    }
+
+    public List<GadgetDataHandler> get() {
+      return handlers;
+    }
+  }
+}
