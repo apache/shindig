6b6561881105f32dd8d7a1b02b4cebff4b431914
Cassandra Doll <doll@apache.org>
2008-05-05 19:11:16 +0000

    SHINDIG-210 - Patch from Vasu Nori. This change cleans up the guice usage within the restul code and the urls now work.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@653560 13f79535-47bb-0310-9956-ffa450edef68

--- java/social-api/src/main/java/org/apache/shindig/social/abdera/ActivitiesServiceAdapter.java
+++ java/social-api/src/main/java/org/apache/shindig/social/abdera/ActivitiesServiceAdapter.java
@@ -17,46 +17,38 @@
 */
 package org.apache.shindig.social.abdera;
 
+import com.google.inject.Inject;
+
+import org.apache.abdera.protocol.server.RequestContext;
+import org.apache.abdera.protocol.server.ResponseContext;
 import org.apache.shindig.social.opensocial.ActivitiesService;
 import org.apache.shindig.social.opensocial.model.Activity;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import org.apache.abdera.protocol.server.RequestContext;
-import org.apache.abdera.protocol.server.ResponseContext;
-
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.logging.Logger;
 
 /**
  * All "activity" requests are processed here.
  *
- * @author vnori@google.com (Vasu Nori)
- *
  */
 @SuppressWarnings("unchecked")
 public class ActivitiesServiceAdapter extends RestServerCollectionAdapter {
-  private static Logger logger =
-    Logger.getLogger(ActivitiesServiceAdapter.class.getName());
   private ActivitiesService handler;
-
+  
   // TODO get these from the config files like in feedserver
   private static final String TITLE = "Acitivity Collection title";
   private static final String AUTHOR = "TODO";
-
-  public ActivitiesServiceAdapter() {
-    // TODO needs cleanup once injection from AbderaServlet works..
-    Injector injector = null;
-    try {
-       injector = Guice.createInjector(new RestGuiceModule());
-    } catch (Exception e) {
-      logger.severe("injector exception: " + e.getMessage());
-    }
-    handler = injector.getInstance(ActivitiesService.class);
+  
+  @Inject
+  public ActivitiesServiceAdapter(ActivitiesService handler) {
+    this.handler = handler;
   }
-
+  
+  /**
+   * Handles the following URL
+   *    /activities/{uid}/@self
+   */
   @Override
   public ResponseContext getFeed(RequestContext request) {
     // get the params from the request
@@ -70,6 +62,10 @@ public class ActivitiesServiceAdapter extends RestServerCollectionAdapter {
     return returnFeed(request, TITLE, AUTHOR, (List)listOfObj);
   }
 
+  /**
+   *  Handles the following URL
+   *    /activities/{uid}/@self/{activityId}
+   */
   @Override
   public ResponseContext getEntry(RequestContext request) {
     // get the params from the request
