ee74c155c7d4ea4e31d9dd34ff2b266f1fd0eb4a
Cassandra Doll <doll@apache.org>
2008-05-06 10:04:44 +0000

    SHINDIG-236 Based on patch from David Primmer. Moves the SocialApiProvider into the abdera subpackage. Cleans up the restful part of the web.xml files.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@653729 13f79535-47bb-0310-9956-ffa450edef68

--- java/social-api/src/main/java/org/apache/shindig/social/abdera/SocialApiProvider.java
+++ java/social-api/src/main/java/org/apache/shindig/social/abdera/SocialApiProvider.java
@@ -15,81 +15,76 @@
  * copyright in this work, please see the NOTICE file in the top level
  * directory of this distribution.
  */
-package org.apache.shindig.social;
+package org.apache.shindig.social.abdera;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-
 import org.apache.abdera.protocol.server.TargetType;
 import org.apache.abdera.protocol.server.impl.DefaultProvider;
 import org.apache.abdera.protocol.server.impl.RouteManager;
-import org.apache.shindig.social.abdera.ActivitiesServiceAdapter;
-import org.apache.shindig.social.abdera.PeopleServiceAdapter;
 
 public class SocialApiProvider extends DefaultProvider {
   //TODO why is this hardcoded here. can't this be from servletContext?
   private static final String BASE = "/social/rest/";
   private static final String ROUTENAME_NOT_USED = "not_used_right_now";
-  private PeopleServiceAdapter peopleAdapter;
-  private ActivitiesServiceAdapter activitiesAdapter;
 
   private Provider<PeopleServiceAdapter> peopleAdapterProvider;
+  private Provider<ActivitiesServiceAdapter> activitiesAdapterProvider;
+
   @Inject
-  public void setPeopleAdapter(
-      Provider<PeopleServiceAdapter> peopleAdapterProvider) {
+  public void setPeopleAdapter(Provider<PeopleServiceAdapter>
+      peopleAdapterProvider) {
     this.peopleAdapterProvider = peopleAdapterProvider;
   }
-  private Provider<ActivitiesServiceAdapter> activitiesAdapterProvider;
+
   @Inject
-  public void setActivitiesAdapter(
-      Provider<ActivitiesServiceAdapter> activitiesAdapterProvider) {
+  public void setActivitiesAdapter(Provider<ActivitiesServiceAdapter>
+      activitiesAdapterProvider) {
     this.activitiesAdapterProvider = activitiesAdapterProvider;
   }
-  
-  public SocialApiProvider() {
-  }
-  
+
   public void initialize() {
-    peopleAdapter = peopleAdapterProvider.get();
-    activitiesAdapter = activitiesAdapterProvider.get();
-    
+    PeopleServiceAdapter peopleAdapter = peopleAdapterProvider.get();
+    ActivitiesServiceAdapter activitiesAdapter
+        = activitiesAdapterProvider.get();
+
     // Add the RouteManager that parses incoming and builds outgoing URLs
     routeManager = new RouteManager()
-    
-      //Collection of all people connected to user {uid} 
+
+      // Collection of all people connected to user {uid}
       // /people/{uid}/@all
       .addRoute(ROUTENAME_NOT_USED,
-            BASE + "people/:uid/@all", 
-            TargetType.TYPE_COLLECTION, 
-            peopleAdapter)
-
-      //Individual person record. /people/{uid}/@all/{pid}
-      .addRoute(ROUTENAME_NOT_USED, 
-          BASE + "people/:uid/@all/:pid", 
-          TargetType.TYPE_ENTRY, 
-          peopleAdapter)
-    
-      //Self Profile record for user {uid} /people/{uid}/@self
-      .addRoute(ROUTENAME_NOT_USED, 
-          BASE + "people/:uid/@self", 
-          TargetType.TYPE_ENTRY, 
+            BASE + "people/:uid/@all",
+            TargetType.TYPE_COLLECTION,
           peopleAdapter)
 
-      //Activities
-      //Collection of activities for given user /activities/{uid}/@self  
-      .addRoute(ROUTENAME_NOT_USED, 
-          BASE + "activities/:uid/@self", 
-          TargetType.TYPE_COLLECTION, 
+      // Individual person record. /people/{uid}/@all/{pid}
+      .addRoute(ROUTENAME_NOT_USED,
+          BASE + "people/:uid/@all/:pid",
+          TargetType.TYPE_ENTRY,
+          peopleAdapter)
+
+      // Self Profile record for user {uid} /people/{uid}/@self
+      .addRoute(ROUTENAME_NOT_USED,
+          BASE + "people/:uid/@self",
+          TargetType.TYPE_ENTRY,
+          peopleAdapter)
+
+      // Activities
+      // Collection of activities for given user /activities/{uid}/@self
+      .addRoute(ROUTENAME_NOT_USED,
+          BASE + "activities/:uid/@self",
+          TargetType.TYPE_COLLECTION,
           activitiesAdapter)
-         
-      //Individual activity resource; usually discovered from collection
+
+      // Individual activity resource; usually discovered from collection
       // /activities/{uid}/@self/{aid}
-      .addRoute(ROUTENAME_NOT_USED, 
-          BASE + "activities/:uid/@self/:aid", 
-          TargetType.TYPE_ENTRY, 
+      .addRoute(ROUTENAME_NOT_USED,
+          BASE + "activities/:uid/@self/:aid",
+          TargetType.TYPE_ENTRY,
           activitiesAdapter)
-    ;    
-      
+    ;
+
     targetBuilder = routeManager;
     targetResolver = routeManager;
   }
