7907e7f6ad54ab71d4a833d7b3cb4f35eb60abfa
Santiago Gala <sgala@apache.org>
2008-05-07 07:10:04 +0000

    Merge branch 'revert_keywords'
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@654012 13f79535-47bb-0310-9956-ffa450edef68

--- etc/set_svn_properties.sh
+++ etc/set_svn_properties.sh
@@ -2,48 +2,40 @@
 # this script will set the proper svn properties on all the files in the tree
 # It pretty much requires a gnu compatible xargs (for the -r flag).  Running
 # on Linux is probably the best option
+# Note: use the folling line if you want to remove svn:keywords
+for ext in java php xml xsl xsd wsdl properties txt htm* css js ; do find . -path '*/.svn' -prune -o  -name "*.$ext" -print0 | xargs -0  -r  svn propdel  svn:keywords ; done
 
 
 find . -path '*/.svn' -prune -o  -name "*.java" -print0 | xargs -0  -r svn propset svn:eol-style native
-find . -path '*/.svn' -prune -o  -name "*.java" -print0 | xargs -0  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "*.php" -print0 | xargs -0  -r svn propset svn:eol-style native
-find . -path '*/.svn' -prune -o  -name "*.php" -print0 | xargs -0  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "*.xml" -print0 | xargs -0  -r  svn propset svn:mime-type text/xml
 find . -path '*/.svn' -prune -o  -name "*.xml" -print0 | xargs -0  -r  svn propset svn:eol-style native
-find . -path '*/.svn' -prune -o  -name "*.xml" -print0 | xargs -0  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "*.xsl" -print0 | xargs -0  -r  svn propset svn:mime-type text/xml
 find . -path '*/.svn' -prune -o  -name "*.xsl" -print0 | xargs -0  -r  svn propset svn:eol-style native
-find . -path '*/.svn' -prune -o  -name "*.xsl" -print0 | xargs -0  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "*.xsd" -print0 | xargs -0  -r  svn propset svn:mime-type text/xml
 find . -path '*/.svn' -prune -o  -name "*.xsd" -print0 | xargs -0  -r  svn propset svn:eol-style native
-find . -path '*/.svn' -prune -o  -name "*.xsd" -print0 | xargs -0  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "*.wsdl" -print0 | xargs -0  -r  svn propset svn:mime-type text/xml
 find . -path '*/.svn' -prune -o  -name "*.wsdl" -print0 | xargs -0  -r  svn propset svn:eol-style native
-find . -path '*/.svn' -prune -o  -name "*.wsdl" -print0 | xargs -0  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "*.properties" -print0 | xargs -0  -r  svn propset svn:mime-type text/plain
 find . -path '*/.svn' -prune -o  -name "*.properties" -print0 | xargs -0  -r  svn propset svn:eol-style native
-find . -path '*/.svn' -prune -o  -name "*.properties" -print0 | xargs -0  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "*.txt" -print0 | xargs -0  -r  svn propset svn:eol-style native
 find . -path '*/.svn' -prune -o  -name "*.txt" -print0 | xargs -0  -r  svn propset svn:mime-type text/plain
 
 find . -path '*/.svn' -prune -o  -name "*.htm*" -print0 | xargs -0  -r  svn propset svn:eol-style native
 find . -path '*/.svn' -prune -o  -name "*.htm*" -print0 | xargs -0  -r  svn propset svn:mime-type text/html
-find . -path '*/.svn' -prune -o  -name "*.htm*" -print0 | xargs -0  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "*.css" -print0 | xargs -0  -r  svn propset svn:eol-style native
 find . -path '*/.svn' -prune -o  -name "*.css" -print0 | xargs -0  -r  svn propset svn:mime-type text/css
-find . -path '*/.svn' -prune -o  -name "*.css" -print0 | xargs -0  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "*.js" -print0 | xargs -0  -r  svn propset svn:eol-style native
 find . -path '*/.svn' -prune -o  -name "*.js" -print0 | xargs -0  -r  svn propset svn:mime-type text/javascript
-find . -path '*/.svn' -prune -o  -name "*.js" -print | grep -v jquery | xargs  -r  svn propset svn:keywords "Author Date Id Revision Rev"
 
 find . -path '*/.svn' -prune -o  -name "README*" -print0 | xargs -0  -r  svn propset svn:eol-style native
 find . -path '*/.svn' -prune -o  -name "README*" -print0 | xargs -0  -r  svn propset svn:mime-type text/plain
