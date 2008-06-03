65a5233cf1ce3123d2102097224e459c5c2bcb98
Chris Chabot <chabotc@apache.org>
2008-06-03 13:43:21 +0000

    SHINDIG-326 : Uncaught exception for invalid request json
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@662798 13f79535-47bb-0310-9956-ffa450edef68

--- php/src/socialdata/http/GadgetDataServlet.php
+++ php/src/socialdata/http/GadgetDataServlet.php
@@ -67,20 +67,19 @@ class GadgetDataServlet extends HttpServlet {
 
 	public function doPost()
 	{
-		$requestParam = isset($_POST['request']) ? $_POST['request'] : '';
-		$token = isset($_POST['st']) ? $_POST['st'] : '';
-		// detect if magic quotes are on, and if so strip them from the request
-		if (get_magic_quotes_gpc()) {
-			$requestParam = stripslashes($requestParam);
-		}
-		$request = json_decode($requestParam, true);
-		if ($request == $requestParam) {
-			// oddly enough if the json_decode function can't parse the code,
-			// it just returns the original string (instead of something usefull like 'null' or false :))
-			throw new Exception("Invalid request JSON");
-		}
-		
 		try {
+			$requestParam = isset($_POST['request']) ? $_POST['request'] : '';
+			$token = isset($_POST['st']) ? $_POST['st'] : '';
+			// detect if magic quotes are on, and if so strip them from the request
+			if (get_magic_quotes_gpc()) {
+				$requestParam = stripslashes($requestParam);
+			}
+			$request = json_decode($requestParam, true);
+			if ($request == $requestParam) {
+				// oddly enough if the json_decode function can't parse the code,
+				// it just returns the original string (instead of something usefull like 'null' or false :))
+				throw new Exception("Invalid request JSON");
+			}
 			$response = new DataResponse($this->createResponse($requestParam, $token));
 		} catch (Exception $e) {
 			$response = new DataResponse(false, BAD_REQUEST);
