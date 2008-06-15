926ebe60cc4da18c0d5c4faa77d147f7d7a33970
Chris Chabot <chabotc@apache.org>
2008-06-15 19:58:23 +0000

    Initial version of REST API support for PHP Shindig.
    This isn't complete yet, so don't try to put this into production just yet ok :)
    
    To try this out, edit your container.js set useRestful : true and set the social data 
    path to /social/rest.
    
    To make sure we don't break anyone's live code during the development of the REST interface
    i've seperated out the old wireformat and rest format into 2 different directories: 
    src/socialdata = the un-touched original wireformat code
    src/socialrest = the new rest code
    
    Unfortunately the internal API's had to be changed to make everything fit with the REST API,
    the bigest change is that i moved away from the single handler class and went (back) to the
    3 Data Service classes (People, AppData and Activities). 
    
    The format is close enough to the old one though that it shouldn't take a lot of effort to
    port your code to the new interface once your ready to switch over.
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@668005 13f79535-47bb-0310-9956-ffa450edef68

--- php/config.php
+++ php/config.php
@@ -24,70 +24,71 @@
  * would be horribly slow (db, xml, ini files etc), so ... here's our config.php
  */
 $shindigConfig = array(
-	// Show debug backtrace? Set this to false on anything that resembles a production env
-	'debug' => false,
+	// Plain text tokens are meant for the html / javascript samples not for hack-me-please type real-life-scenarios
+	// Set both of these to false on anything resembling a live deployment
+	'debug' => true, 
+	'allow_plaintext_token' => true, 
 
 	// The base prefix under which the our url's live, if its the root set this to ''
 	// don't forget to update your .htaccess to reflect this, as well as your container 
-	// javascript like: gadget.setServerBase('/someBaseUrl/');
-	'web_prefix' => '',
+	// javascript like: gadget.setServerBase('/someBaseUrl');
+	'web_prefix' => '', 
 
 	// Max age of a security token, defaults to one hour
-	'st_max_age' => 60 * 60,
-
+	'st_max_age' => 60 * 60, 
+	
 	// Security token keys
 	'token_cipher_key' => 'INSECURE_DEFAULT_KEY',
-	'token_hmac_key' => 'INSECURE_DEFAULT_KEY',
-
+	'token_hmac_key' => 'INSECURE_DEFAULT_KEY', 
+		
 	// Compresses features javascript? can save upto 50% page size
-	'compress_javascript' => true,
-
+	'compress_javascript' => true, 
+	
 	// Configurable CSS rules that are injected to the gadget page, 
 	// be careful when adjusting these not to break most gadget's layouts :)
+	//'gadget_css' => 'body,td,div,span,p{font-family:arial,sans-serif;} body {background-color:#ffffff; font-family: arial, sans-serif; padding: 0px; margin: 0px;  font-size: 12px; color: #000000;}a, a:visited {color: #3366CC;text-decoration: none; }a:hover {color: #3366CC; text-decoration: underline;} input, select { border: 1px solid #bdc7d8;font-size: 11px;padding: 3px;}', 
 	'gadget_css' => 'body,td,div,span,p{font-family:arial,sans-serif;} a {color:#0000cc;}a:visited {color:#551a8b;}a:active {color:#ff0000;}body{margin: 0px;padding: 0px;background-color:white;}',
-	//'gadget_css' => 'body,td,div,span,p{font-family:arial,sans-serif;} body {background-color:#ffffff; font-family: arial, sans-serif; padding: 0px; margin: 0px;  font-size: 12px; color: #000000;}a, a:visited {color: #3366CC;text-decoration: none; }a:hover {color: #3366CC; text-decoration: underline;} input, select { border: 1px solid #bdc7d8;font-size: 11px;padding: 3px;}',
-	
-	// The html / javascript samples use a plain text demo token,
-	// set this to false on anything resembling a real site
-	'allow_plaintext_token' => true,
 	
 	// P3P (Platform for Privacy Preferences) header for allowing cross domain cookies.
 	// Setting this to an empty string: '' means no P3P header will be send
-	'P3P' => 'CP="CAO PSA OUR"',
-
+	'P3P' => 'CP="CAO PSA OUR"', 
+	
 	// location of the features directory on disk. The default setting assumes you did a 
 	// full checkout of the shindig project, and not just the php part.
 	// Otherwise also checkout the features, config and javascript directories and set
 	// these to their locations
-	'features_path' => realpath(dirname(__FILE__)) . '/../features/',
-	'container_path' => realpath(dirname(__FILE__)) . '/../config/',
+	'features_path' => realpath(dirname(__FILE__)) . '/../features/', 
+	'container_path' => realpath(dirname(__FILE__)) . '/../config/', 
 	'javascript_path' => realpath(dirname(__FILE__)) . '/../javascript/', 
-	'container_config' => realpath(dirname(__FILE__)) . '/../config/container.js',
-
-	// The data handlers for the social data, this is a list of class names
-	// seperated by a , For example:
-	//'handlers' => 'PartuzaHandler',
-	// if the value is empty, the defaults used in the example above will be used.
+	'container_config' => realpath(dirname(__FILE__)) . '/../config/container.js', 
+	
+	//WARNING: handlers is depreciated in the RESTful protocol, use people/activity and appdata service instead
+	// 'handlers' => 'PartuzaHandler',
 	'handlers' => '',
 
-	'focedJsLibs' => '',
-	
+	// REST API service adapters
+	'people_service' => 'BasicPeopleService',
+	'activity_service' => 'BasicActivitiesService',
+	'app_data_service' => 'BasicAppDataService',		
+
+	'focedJsLibs' => '', 
+
 	// Configurable classes to use, this way we provide extensibility for what 
 	// backends the gadget server uses for its logic functionality. 
-	'blacklist_class' => 'BasicGadgetBlacklist',
-	'remote_content' => 'BasicRemoteContent',
-	'security_token_signer' => 'BasicSecurityTokenDecoder',
-	'security_token' => 'BasicSecurityToken',
+	'blacklist_class' => 'BasicGadgetBlacklist', 
+	'remote_content' => 'BasicRemoteContent', 
+	'security_token_signer' => 'BasicSecurityTokenDecoder', 
+	'security_token' => 'BasicSecurityToken', 
 	'data_cache' => 'CacheFile', 
 	
 	// gadget server specific settings
 	'userpref_param_prefix' => 'up_',
-	'libs_param_name' => 'libs',
+	'libs_param_name' => 'libs', 
 	// location  of the javascript handler (include the full path), default this is /gadgets/js
-	'default_js_prefix' => '/gadgets/js/',
+	'default_js_prefix' => '/gadgets/js/', 
 	// location of the gadget iframe renderer, default this is /gadgets/ifr?
 	'default_iframe_prefix' => '/gadgets/ifr?', 
-	
+
 	// if your using memcached, these values are used for locating the server
 	// if your not using memcached, ignore these values
 	'cache_host' => 'localhost',
@@ -96,14 +97,15 @@ $shindigConfig = array(
 	// global cache age policy and location
 	'cache_time' => 24 * 60 * 60,
 	'cache_root' => '/tmp/shindig', 
-
-	// OAuth private key Path
-	'private_key_file' => realpath(dirname(__FILE__)) . '/certs/private.key',
-	// file path to public RSA cert
-	'public_key_file' => realpath(dirname(__FILE__)) . '/certs/public.crt',
-	// Phrase to decrypt private key. Leave empty if unencrypted
-	'private_key_phrase' => 'partuza',
 	
+	// See certs/README on how to generate these keys
+	// OAuth private key path
+	'private_key_file' => realpath(dirname(__FILE__)) . '/certs/private.key', 
+	// OAuth public key path
+	'public_key_file' => realpath(dirname(__FILE__)) . '/certs/public.crt', 
+	// Phrase to decrypt private key. Leave empty if unencrypted
+	'private_key_phrase' => 'partuza', 
+
 	// In some cases we need to know the site root (for features forinstance)
 	'base_path' => realpath(dirname(__FILE__))
 );
@@ -115,6 +117,7 @@ class ConfigException extends Exception {}
  * not so pretty $shindigConfig array some day.
  */
 class Config {
+
 	static function get($key)
 	{
 		global $shindigConfig;
@@ -128,7 +131,7 @@ class Config {
 	static function set($key, $val)
 	{
 		global $shindigConfig;
-			if (isset($shindigConfig[$key])) {
+		if (isset($shindigConfig[$key])) {
 			$shindigConfig[$key] = $val;
 		} else {
 			throw new ConfigException("Invalid Config Key");
---
+++ php/src/socialrest/ActivityHandler.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+class ActivityHandler extends DataRequestHandler {
+	private $service;
+
+	public function __construct()
+	{
+		$service = Config::get('activity_service');
+		$this->service = new $service();
+	}
+
+	public function handleDelete($params, $token)
+	{
+		return new ResponseItem(BAD_REQUEST, "You can't delete activities. ", null);
+	}
+
+	/**
+	 * /activities/{userId}/{groupId}/{optionalActvityId}
+	 *
+	 * examples:
+	 * /activities/john.doe/@self/1
+	 * /activities/john.doe/@self
+	 * /activities/john.doe/@friends
+	 */
+	public function handleGet($params, $token)
+	{
+		$userId = UserId::fromJson($params[1]);
+		$groupId = GroupId::fromJson($params[2]);
+		$optionalActivityId = null;
+		if (count($params) > 3) {
+			$optionalActivityId = $params[3];
+		}
+		// TODO: Filter by fields
+		// TODO: do we need to add pagination and sorting support?
+		if ($optionalActivityId != null) {
+			return $this->service->getActivity($userId, $groupId, $optionalActivityId, $token);
+		}
+		return $this->service->getActivities($userId, $groupId, $token);
+	}
+
+	/**
+	 * /activities/{userId}/@self
+	 *
+	 * examples:
+	 * /activities/john.doe/@self
+	 * - postBody is an activity object
+	 */
+	public function handlePost($params, $token)
+	{
+		$userId = UserId::fromJson($params[1]);
+		$groupId = GroupId::fromJson($params[2]);
+		// TODO: Should we pass the groupId through to the service?
+		$jsonActivity = isset($_POST['entry']) ? $_POST['entry'] : (isset($_GET['entry']) ? $_GET['entry'] : null);
+		$activity = $this->convertToObject($jsonActivity);
+    	return $this->service->createActivity($userId, $activity, $token);
+	}
+
+	/**
+	 * /activities/{userId}/@self
+	 *
+	 * examples:
+	 * /activities/john.doe/@self
+	 * - postBody is an activity object
+	 */
+	public function handlePut($params, $token)
+	{
+		return $this->handlePost($token);
+	}
+}
