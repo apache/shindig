3bf250f3d27384171c5191422730346e7cee5656
Chris Chabot <chabotc@apache.org>
2008-06-25 23:50:44 +0000

    Phase 1 of the config rewrite. Configuration values moved to config/container.php and you can put your local site values in config/local.php. This way you can upgrade/svn update without loosing your local config
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@671694 13f79535-47bb-0310-9956-ffa450edef68

--- php/config.php
+++ php/config.php
@@ -18,127 +18,38 @@
  * 
  */
 
-/*
- * I really detest such config files to be honest, why put configuration in a web document! 
- * But since PHP lacks a propper way to set application configurations, and any other method 
- * would be horribly slow (db, xml, ini files etc), so ... here's our config.php
- */
-$shindigConfig = array(
-	// Plain text tokens are meant for the html / javascript samples not for hack-me-please type real-life-scenarios
-	// Set both of these to false on anything resembling a live deployment
-	'debug' => true, 
-	'allow_plaintext_token' => true, 
-
-	// The base prefix under which the our url's live, if its the root set this to ''
-	// don't forget to update your .htaccess to reflect this, as well as your container 
-	// javascript like: gadget.setServerBase('/someBaseUrl');
-	'web_prefix' => '', 
-
-	// Max age of a security token, defaults to one hour
-	'st_max_age' => 60 * 60, 
-	
-	// Security token keys
-	'token_cipher_key' => 'INSECURE_DEFAULT_KEY',
-	'token_hmac_key' => 'INSECURE_DEFAULT_KEY', 
-		
-	// Compresses features javascript? can save upto 50% page size
-	'compress_javascript' => true, 
-	
-	// Configurable CSS rules that are injected to the gadget page, 
-	// be careful when adjusting these not to break most gadget's layouts :)
-	//'gadget_css' => 'body,td,div,span,p{font-family:arial,sans-serif;} body {background-color:#ffffff; font-family: arial, sans-serif; padding: 0px; margin: 0px;  font-size: 12px; color: #000000;}a, a:visited {color: #3366CC;text-decoration: none; }a:hover {color: #3366CC; text-decoration: underline;} input, select { border: 1px solid #bdc7d8;font-size: 11px;padding: 3px;}', 
-	'gadget_css' => 'body,td,div,span,p{font-family:arial,sans-serif;} a {color:#0000cc;}a:visited {color:#551a8b;}a:active {color:#ff0000;}body{margin: 0px;padding: 0px;background-color:white;}',
-	
-	// P3P (Platform for Privacy Preferences) header for allowing cross domain cookies.
-	// Setting this to an empty string: '' means no P3P header will be send
-	'P3P' => 'CP="CAO PSA OUR"', 
-	
-	// location of the features directory on disk. The default setting assumes you did a 
-	// full checkout of the shindig project, and not just the php part.
-	// Otherwise also checkout the features, config and javascript directories and set
-	// these to their locations
-	'features_path' => realpath(dirname(__FILE__)) . '/../features/', 
-	'container_path' => realpath(dirname(__FILE__)) . '/../config/', 
-	'javascript_path' => realpath(dirname(__FILE__)) . '/../javascript/', 
-	'container_config' => realpath(dirname(__FILE__)) . '/../config/container.js', 
-	
-	//WARNING: handlers is depreciated in the RESTful protocol, use people/activity and appdata service instead
-	// 'handlers' => 'PartuzaHandler',
-	'handlers' => '',
-
-	// REST API service adapters
-	'people_service' => 'BasicPeopleService',
-	'activity_service' => 'BasicActivitiesService',
-	'app_data_service' => 'BasicAppDataService',		
-
-	'focedJsLibs' => '', 
-
-	// Configurable classes to use, this way we provide extensibility for what 
-	// backends the gadget server uses for its logic functionality. 
-	'blacklist_class' => 'BasicGadgetBlacklist', 
-	'remote_content' => 'BasicRemoteContent', 
-	'security_token_signer' => 'BasicSecurityTokenDecoder', 
-	'security_token' => 'BasicSecurityToken', 
-	'data_cache' => 'CacheFile', 
-	
-	// gadget server specific settings
-	'userpref_param_prefix' => 'up_',
-	'libs_param_name' => 'libs', 
-	// location  of the javascript handler (include the full path), default this is /gadgets/js
-	'default_js_prefix' => '/gadgets/js/', 
-	// location of the gadget iframe renderer, default this is /gadgets/ifr?
-	'default_iframe_prefix' => '/gadgets/ifr?', 
-
-	// if your using memcached, these values are used for locating the server
-	// if your not using memcached, ignore these values
-	'cache_host' => 'localhost',
-	'cache_port' => 11211, 
-	
-	// global cache age policy and location
-	'cache_time' => 24 * 60 * 60,
-	'cache_root' => '/tmp/shindig', 
-	
-	// Some people develop living behind a proxy server, setting this to your
-	// proxy's url (proxy.mycompany.com) will make curl use that proxy.
-	// On production systems you would not want to do this since it would mess up
-	// the refreshInterval and caching policies of shindig
-	'proxy' => '',
-
-	// See certs/README on how to generate these keys
-	// OAuth private key path
-	'private_key_file' => realpath(dirname(__FILE__)) . '/certs/private.key', 
-	// OAuth public key path
-	'public_key_file' => realpath(dirname(__FILE__)) . '/certs/public.crt', 
-	// Phrase to decrypt private key. Leave empty if unencrypted
-	'private_key_phrase' => 'partuza', 
-
-	// In some cases we need to know the site root (for features forinstance)
-	'base_path' => realpath(dirname(__FILE__))
-);
-
 class ConfigException extends Exception {}
 
 /**
- * Abstracts how to retrieve configuration values so we can replace the
- * not so pretty $shindigConfig array some day.
+ * Configuration class. It uses the keys/values from config/container.php
+ * and (if the file exists) config/local.php.
  */
 class Config {
-
-	static function get($key)
+	static private $config = false;
+	
+	static private function loadConfig()
 	{
 		global $shindigConfig;
-		if (isset($shindigConfig[$key])) {
-			return $shindigConfig[$key];
-		} else {
-			throw new ConfigException("Invalid Config Key");
+		if (!self::$config) {
+			// load default configuration
+			include_once 'config/container.php';
+			self::$config = $shindigConfig;
+			if (file_exists('config/local.php')) {
+				// include local.php if it exists and merge the config arrays. 
+				// the second array values overwrites the first one's
+				include_once 'config/local.php';
+				self::$config = array_merge(self::$config, $shindigConfig);
+			}
 		}
 	}
-
-	static function set($key, $val)
+	
+	static function get($key)
 	{
-		global $shindigConfig;
-		if (isset($shindigConfig[$key])) {
-			$shindigConfig[$key] = $val;
+		if (!self::$config) {
+			self::loadConfig();
+		}
+		if (isset(self::$config[$key])) {
+			return self::$config[$key];
 		} else {
 			throw new ConfigException("Invalid Config Key");
 		}
