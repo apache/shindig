3db356c0ec9e13e9170b12af9a6572c493449ac5
Ian Boston <ieb@apache.org>
2008-07-24 11:54:24 +0000

    More Javadoc on the model, some fixes to the doc in person.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@679376 13f79535-47bb-0310-9956-ffa450edef68

--- java/social-api/src/main/java/org/apache/shindig/social/opensocial/model/Email.java
+++ java/social-api/src/main/java/org/apache/shindig/social/opensocial/model/Email.java
@@ -22,36 +22,74 @@ import org.apache.shindig.social.core.model.EmailImpl;
 import com.google.inject.ImplementedBy;
 
 /**
- * see
- * http://code.google.com/apis/opensocial/docs/0.7/reference/opensocial.Email.Field.html
+ * see http://code.google.com/apis/opensocial/docs/0.7/reference/opensocial.Email.Field.html
  *
  */
 
 @ImplementedBy(EmailImpl.class)
-
 public interface Email {
 
+  /**
+   * The fields that represent the email object ion json form.
+   */
   public static enum Field {
+    /** the json field for address. */
     ADDRESS("address"),
+    /** the json field for type. */
     TYPE("type");
 
+    /**
+     * The json field that the instance represents.
+     */
     private final String jsonString;
 
+    /**
+     * create a field base on the a json element.
+     *
+     * @param jsonString the name of the element
+     */
     private Field(String jsonString) {
       this.jsonString = jsonString;
     }
 
+    /**
+     * emit the field as a json element.
+     *
+     * @return the field name
+     */
     @Override
     public String toString() {
       return this.jsonString;
     }
   }
 
+  /**
+   * The email address, specified as a string.
+   *
+   * @return email address
+   */
   String getAddress();
 
+  /**
+   * The email address, specified as a string.
+   *
+   * @param address email address
+   */
   void setAddress(String address);
 
+  /**
+   * The email type or label, specified as a string. Examples: work, my favorite store, my house,
+   * etc.
+   *
+   * @return email type or label
+   */
   String getType();
 
+  /**
+   * The email type or label, specified as a string. Examples: work, my favorite store, my house,
+   * etc.
+   *
+   * @param type email type or label
+   */
   void setType(String type);
 }
