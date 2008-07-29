fa81a14589af99651f4d2d4b04ed63c77a10c8aa
Cassandra Doll <doll@apache.org>
2008-07-29 00:35:34 +0000

    Added a helper method to Person.Field to translate from url strings into the Person.Field enum.
    Fixed the PersonHandler to point to the Person.Field default fields enum instead of having a local copy.
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@680583 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/social-api/src/test/java/org/apache/shindig/social/opensocial/model/PersonTest.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.apache.shindig.social.opensocial.model;
+
+import junit.framework.TestCase;
+
+public class PersonTest extends TestCase {
+
+  public void testInvalidFromUrlString() throws Exception {
+    assertNull(Person.Field.fromUrlString("badness"));
+  }
+
+  public void testFromUrlString() throws Exception {
+    assertUrlStringMaps(Person.Field.NAME);
+    assertUrlStringMaps(Person.Field.THUMBNAIL_URL);
+  }
+
+  private void assertUrlStringMaps(Person.Field field) {
+    assertEquals(field, Person.Field.fromUrlString(field.toString()));
+  }
+
+}
