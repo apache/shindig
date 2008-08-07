3f286a6a194b1a8f4ca62e53fd8279af88b081bf
Cassandra Doll <doll@apache.org>
2008-08-07 20:24:19 +0000

    Replaced the AuthenticationServletFilter impl with a simple dispatch to the AuthenticationHandlers.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@683700 13f79535-47bb-0310-9956-ffa450edef68

--- java/social-api/src/main/java/org/apache/shindig/social/core/config/SocialApiGuiceModule.java
+++ java/social-api/src/main/java/org/apache/shindig/social/core/config/SocialApiGuiceModule.java
@@ -19,25 +19,16 @@
 package org.apache.shindig.social.core.config;
 
 import org.apache.shindig.common.servlet.ParameterFetcher;
-import org.apache.shindig.social.core.oauth.AuthenticationServletFilter;
-import org.apache.shindig.social.core.oauth.BasicOAuthConsumerStore;
-import org.apache.shindig.social.core.oauth.BasicOAuthTokenPrincipalMapper;
-import org.apache.shindig.social.core.oauth.BasicOAuthTokenStore;
+import org.apache.shindig.social.core.oauth.AnonymousAuthenticationHandler;
 import org.apache.shindig.social.core.util.BeanJsonConverter;
 import org.apache.shindig.social.core.util.BeanXmlConverter;
-import org.apache.shindig.social.opensocial.oauth.OAuthConsumerStore;
-import org.apache.shindig.social.opensocial.oauth.OAuthTokenPrincipalMapper;
-import org.apache.shindig.social.opensocial.oauth.OAuthTokenStore;
 import org.apache.shindig.social.opensocial.service.BeanConverter;
 import org.apache.shindig.social.opensocial.service.DataServiceServletFetcher;
 import org.apache.shindig.social.opensocial.service.HandlerProvider;
 import org.apache.shindig.social.sample.service.SampleContainerHandlerProvider;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Scopes;
 import com.google.inject.name.Names;
-import net.oauth.OAuthValidator;
-import net.oauth.SimpleOAuthValidator;
 
 /**
  * Provides social api component injection.
@@ -55,23 +46,13 @@ public class SocialApiGuiceModule extends AbstractModule {
     bind(String.class).annotatedWith(Names.named("canonical.json.db"))
         .toInstance("sampledata/canonicaldb.json");
 
-    // OAuth configuration
-
     bind(Boolean.class)
-        .annotatedWith(Names.named(AuthenticationServletFilter.ALLOW_UNAUTHENTICATED))
+        .annotatedWith(Names.named(AnonymousAuthenticationHandler.ALLOW_UNAUTHENTICATED))
         .toInstance(Boolean.TRUE);
 
-    bind(OAuthValidator.class).to(SimpleOAuthValidator.class);
-    bind(OAuthTokenStore.class)
-        .to(BasicOAuthTokenStore.class).in(Scopes.SINGLETON);
-    bind(OAuthConsumerStore.class)
-        .to(BasicOAuthConsumerStore.class).in(Scopes.SINGLETON);
-    bind(OAuthTokenPrincipalMapper.class)
-        .to(BasicOAuthTokenPrincipalMapper.class).in(Scopes.SINGLETON);
     bind(BeanConverter.class).annotatedWith(Names.named("bean.converter.xml")).to(
         BeanXmlConverter.class);
     bind(BeanConverter.class).annotatedWith(Names.named("bean.converter.json")).to(
         BeanJsonConverter.class);
   }
-
 }
