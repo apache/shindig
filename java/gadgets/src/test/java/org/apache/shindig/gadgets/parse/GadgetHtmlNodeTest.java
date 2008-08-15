6bdc86c4f7df2fec17d5dff85d31ec89a1bd30b0
John Hjelmstad <johnh@apache.org>
2008-08-15 01:48:26 +0000

    Initial implementation of the classes involved in manipulating mutable gadget HTML and CSS.
    Each class can be created from a Parsed equivalent of itself, or freshly created in-memory.
    The HTML representation is a sort of DOM-lite, while CSS manipulation consists of ordered-string
    (selector) manipulation and String/String Map manipulation for declarations. Tests are provided
    for each creation mechanism and all APIs.
    
    This implementation will likely change notably as I and others attempt to put it to real use in
    rewriters. Hopefully modifying it will be reasonably easy due to its being abstracted away
    from parsing logic.
    
    This commit resolves SHINDIG-524, and does not affect any existing functionality yet.
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@686106 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/gadgets/src/test/java/org/apache/shindig/gadgets/parse/GadgetHtmlNodeTest.java
@@ -0,0 +1,421 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.apache.shindig.gadgets.parse;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.replay;
+
+import org.easymock.classextension.EasyMock;
+
+import junit.framework.TestCase;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class GadgetHtmlNodeTest extends TestCase {
+  private ParsedHtmlNode makeTagNodeFromParsed(
+      String tag, String[][] attribs, ParsedHtmlNode[] children) {
+    ParsedHtmlNode parsed = EasyMock.createNiceMock(ParsedHtmlNode.class);
+    expect(parsed.getTagName()).andReturn(tag).anyTimes();
+    List<ParsedHtmlAttribute> attributes = new LinkedList<ParsedHtmlAttribute>();
+    if (attribs != null) {
+      for (String[] attrib : attribs) {
+        attributes.add(
+            GadgetHtmlAttributeTest.makeParsedAttribute(attrib[0], attrib[1]));
+      }
+    }
+    expect(parsed.getAttributes()).andReturn(attributes).anyTimes();
+    List<ParsedHtmlNode> childNodes = new LinkedList<ParsedHtmlNode>();
+    if (children != null) {
+      for (ParsedHtmlNode child : children) {
+        childNodes.add(child);
+      }
+    }
+    expect(parsed.getChildren()).andReturn(childNodes).anyTimes();
+    expect(parsed.getText()).andReturn(null).anyTimes();
+    replay(parsed);
+    return parsed;
+  }
+  
+  private GadgetHtmlNode makeTagNodeFromNew(
+      String tag, String[][] attribs, GadgetHtmlNode[] children) {
+    GadgetHtmlNode node = new GadgetHtmlNode(tag, attribs);
+    if (children != null) {
+      for (GadgetHtmlNode child : children) {
+        node.appendChild(child);
+      }
+    }
+    return node;
+  }
+  
+  private ParsedHtmlNode makeTextNodeFromParsed(String text) {
+    ParsedHtmlNode parsed = EasyMock.createNiceMock(ParsedHtmlNode.class);
+    expect(parsed.getText()).andReturn(text).anyTimes();
+    expect(parsed.getTagName()).andReturn(null).anyTimes();
+    expect(parsed.getAttributes()).andReturn(null).anyTimes();
+    expect(parsed.getChildren()).andReturn(null).anyTimes();
+    replay(parsed);
+    return parsed;
+  }
+  
+  private GadgetHtmlNode makeTextNodeFromNew(String text) {
+    return new GadgetHtmlNode(text);
+  }
+  
+  // Test: tag tree
+  private static String[][] tagTreeAttribs = { { "id", "foo" }, { "name", "bar" } };
+  public void testTagTreeCreatedFromParsedGetters() {
+    ParsedHtmlNode[] parsedKids = {
+        makeTextNodeFromParsed("content"),
+        makeTagNodeFromParsed("span", null, null)
+    };
+    ParsedHtmlNode parsed = makeTagNodeFromParsed("div", tagTreeAttribs, parsedKids);
+    GadgetHtmlNode node = new GadgetHtmlNode(parsed);
+    validateTagTreeGetters(node);
+  }
+  
+  public void testTagTreeCreatedByNewGetters() {
+    GadgetHtmlNode[] children = {
+        makeTextNodeFromNew("content"),
+        makeTagNodeFromNew("span", null, null)
+    };
+    GadgetHtmlNode node = makeTagNodeFromNew("div", tagTreeAttribs, children);
+    validateTagTreeGetters(node);
+  }
+  
+  private void validateTagTreeGetters(GadgetHtmlNode node) {
+    assertFalse(node.isText());
+    assertEquals("div", node.getTagName());
+    assertNull(node.getParentNode());
+    
+    Set<String> attribKeys = node.getAttributeKeys();
+    assertEquals(2, attribKeys.size());
+    assertTrue(attribKeys.contains("id"));
+    assertTrue(attribKeys.contains("name"));
+    assertTrue(node.hasAttribute("id"));
+    assertEquals("foo", node.getAttributeValue("id"));
+    assertTrue(node.hasAttribute("name"));
+    assertEquals("bar", node.getAttributeValue("name"));
+    
+    List<GadgetHtmlNode> children = node.getChildren();
+    assertEquals(2, children.size());
+    
+    GadgetHtmlNode textChild = children.get(0);
+    assertTrue(textChild.isText());
+    assertEquals("content", textChild.getText());
+    assertSame(node, textChild.getParentNode());
+    
+    GadgetHtmlNode tagChild = children.get(1);
+    assertFalse(tagChild.isText());
+    assertEquals("span", tagChild.getTagName());
+    assertNotNull(tagChild.getAttributeKeys());
+    assertEquals(0, tagChild.getAttributeKeys().size());
+    assertNotNull(tagChild.getChildren());
+    assertEquals(0, tagChild.getChildren().size());
+    assertSame(node, tagChild.getParentNode());
+  }
+  
+  // Test: basic getters
+  private static String textGetterContent = "content";
+  public void testTextCreatedFromParsedGetters() {
+    ParsedHtmlNode parsed = makeTextNodeFromParsed(textGetterContent);
+    GadgetHtmlNode node = new GadgetHtmlNode(parsed);
+    validateTextGetters(node);
+  }
+  
+  public void testTextCreatedByNewGetters() {
+    validateTextGetters(makeTextNodeFromNew(textGetterContent));
+  }
+  
+  private void validateTextGetters(GadgetHtmlNode node) {
+    assertTrue(node.isText());
+    assertEquals("content", node.getText());
+    assertNull(node.getParentNode());
+  }
+  
+  // Test: tag name setter
+  public void testTagCreatedFromParsedTagSetter() {
+    ParsedHtmlNode parsed = makeTagNodeFromParsed("div", null, null);
+    GadgetHtmlNode node = new GadgetHtmlNode(parsed);
+    validateTagNameSetter(node);
+  }
+  
+  public void testTagCreatedFromNewTagSetter() {
+    validateTagNameSetter(makeTagNodeFromNew("div", null, null));
+  }
+  
+  private void validateTagNameSetter(GadgetHtmlNode node) {
+    assertFalse(node.isText());
+    assertEquals("div", node.getTagName());
+    node.setTagName("span");
+    assertEquals("span", node.getTagName());
+  }
+  
+  // Test: tag attribute manipulation
+  private static String[][] tagManipAttribs = { { "id", "foo" } };
+  public void testTagCreatedFromParsedAttributeManipulation() {
+    ParsedHtmlNode parsed =
+        makeTagNodeFromParsed("div", tagManipAttribs, null);
+    GadgetHtmlNode node = new GadgetHtmlNode(parsed);
+    validateTagAttributeManipulation(node);
+  }
+  
+  public void testTagCreatedFromNewAttributeManipulation() {
+    validateTagAttributeManipulation(
+        makeTagNodeFromNew("div", tagManipAttribs, null));
+  }
+  
+  private void validateTagAttributeManipulation(GadgetHtmlNode node) {
+    assertFalse(node.isText());
+    
+    Set<String> origKeys = node.getAttributeKeys();
+    assertNotNull(origKeys);
+    assertEquals(1, origKeys.size());
+    assertTrue(origKeys.contains("id"));
+    assertTrue(node.hasAttribute("id"));
+    assertEquals("foo", node.getAttributeValue("id"));
+    
+    // Set existing key
+    node.setAttribute("id", "bar");
+    Set<String> barKeys = node.getAttributeKeys();
+    assertNotNull(barKeys);
+    assertEquals(1, barKeys.size());
+    assertTrue(barKeys.contains("id"));
+    assertTrue(node.hasAttribute("id"));
+    assertEquals("bar", node.getAttributeValue("id"));
+    
+    // Set existing key to null: null's perfectly valid
+    // Also set with whitespace padding to test that bit
+    assertTrue(node.setAttribute("id", null));
+    Set<String> nullKeys = node.getAttributeKeys();
+    assertNotNull(nullKeys);
+    assertEquals(1, nullKeys.size());
+    assertTrue(nullKeys.contains("id"));
+    assertTrue(node.hasAttribute("id"));
+    assertEquals(null, node.getAttributeValue("id"));
+    
+    // Remove id key
+    assertTrue(node.removeAttribute("id"));
+    assertFalse(node.removeAttribute("id"));
+    Set<String> noKeys = node.getAttributeKeys();
+    assertNotNull(noKeys);
+    assertEquals(0, noKeys.size());
+    assertFalse(node.hasAttribute("id"));
+    
+    // Add some new key
+    assertTrue(node.setAttribute("name", "value"));
+    Set<String> newKeys = node.getAttributeKeys();
+    assertNotNull(newKeys);
+    assertEquals(1, newKeys.size());
+    assertTrue(node.hasAttribute("name"));
+    assertEquals("value", node.getAttributeValue("name"));
+    assertTrue(node.removeAttribute("name"));
+    assertFalse(node.removeAttribute("name"));
+  }
+  
+  // Test: tag node manipulation
+  // We don't do created-from-new testing since html node creation types are
+  // already mixed here.
+  public void testTagCreatedFromParsedNodeManipulation() {
+    ParsedHtmlNode[] kidNodes = { makeTextNodeFromParsed("content") };
+    ParsedHtmlNode parsed = makeTagNodeFromParsed("div", null, kidNodes);
+    GadgetHtmlNode parentNode = new GadgetHtmlNode(parsed);
+    
+    // Sanity check on created child
+    List<GadgetHtmlNode> initialChildren = parentNode.getChildren();
+    assertNotNull(initialChildren);
+    assertEquals(1, initialChildren.size());
+    GadgetHtmlNode textNode = initialChildren.get(0);
+    assertTrue(textNode.isText());
+    assertSame(parentNode, textNode.getParentNode());
+    
+    // appendChild
+    GadgetHtmlNode afterNode = new GadgetHtmlNode(makeTagNodeFromParsed("after", null, null));
+    parentNode.appendChild(afterNode);
+    List<GadgetHtmlNode> appendKids = parentNode.getChildren();
+    assertNotNull(appendKids);
+    assertEquals(2, appendKids.size());
+    assertSame(textNode, appendKids.get(0));
+    assertSame(afterNode, appendKids.get(1));
+    assertSame(parentNode, textNode.getParentNode());
+    assertSame(parentNode, afterNode.getParentNode());
+    
+    // insertBefore
+    GadgetHtmlNode beforeNode = new GadgetHtmlNode(makeTagNodeFromParsed("before", null, null));
+    parentNode.insertBefore(beforeNode, textNode);
+    List<GadgetHtmlNode> insertKids = parentNode.getChildren();
+    assertNotNull(insertKids);
+    assertEquals(3, insertKids.size());
+    assertSame(beforeNode, insertKids.get(0));
+    assertSame(textNode, insertKids.get(1));
+    assertSame(afterNode, insertKids.get(2));
+    assertSame(parentNode, beforeNode.getParentNode());
+    assertSame(parentNode, textNode.getParentNode());
+    assertSame(parentNode, afterNode.getParentNode());
+    
+    // remove before and text, leaving only after
+    assertTrue(parentNode.removeChild(beforeNode));
+    assertFalse(parentNode.removeChild(beforeNode));
+    assertTrue(parentNode.removeChild(textNode));
+    assertFalse(parentNode.removeChild(textNode));
+    assertNull(beforeNode.getParentNode());
+    assertNull(textNode.getParentNode());
+    List<GadgetHtmlNode> remainingKids = parentNode.getChildren();
+    assertNotNull(remainingKids);
+    assertEquals(1, remainingKids.size());
+    assertSame(parentNode, afterNode.getParentNode());
+    assertSame(afterNode, remainingKids.get(0));
+  }
+  
+  // Test: text setter
+  public void testTextCreatedFromParsedTextSetter() {
+    ParsedHtmlNode parsed = makeTextNodeFromParsed("content");
+    GadgetHtmlNode node = new GadgetHtmlNode(parsed);
+    validateTextSetter(node);
+  }
+  
+  public void tesetTextCreatedFromNewTextSetter() {
+    validateTextSetter(makeTextNodeFromNew("content"));
+  }
+  
+  private void validateTextSetter(GadgetHtmlNode node) {
+    assertTrue(node.isText());
+    assertEquals("content", node.getText());
+    node.setText("new content");
+    assertEquals("new content", node.getText());
+  }
+  
+  // Test: text-node API limitation
+  public void testTagsFromParsedCantUseTextApis() {
+    validateTagsCantUseTextApis(
+        new GadgetHtmlNode(makeTagNodeFromParsed("tag", null, null)));
+  }
+  
+  public void testTagsFromNewCantUseTextApis() {
+    validateTagsCantUseTextApis(makeTagNodeFromNew("tag", null, null));
+  }
+  
+  private void validateTagsCantUseTextApis(GadgetHtmlNode tagNode) {
+    assertFalse(tagNode.isText());
+    
+    try {
+      tagNode.getText();
+      fail("Tag nodes shouldn't be able to use getText()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      tagNode.setText("foo");
+      fail("Tag nodes shouldn't be able to use setText()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+  }
+  
+  // Test: tag-node API limitation
+  public void testTextFromParsedCantUseTagsApis() {
+    validateTextCantUseTagsApis(new GadgetHtmlNode(makeTextNodeFromParsed("content")));
+  }
+  
+  public void testTextFromNewCantUseTagsApis() {
+    validateTextCantUseTagsApis(makeTextNodeFromNew("content"));
+  }
+  
+  private void validateTextCantUseTagsApis(GadgetHtmlNode textNode) {    
+    assertTrue(textNode.isText());
+    
+    try {
+      textNode.appendChild(null);
+      fail("Text nodes shouldn't be able to use appendChild()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.getAttributeKeys();
+      fail("Text nodes shouldn't be able to use getAttributeKeys()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.getAttributeValue("foo");
+      fail("Text nodes shouldn't be able to use getAttributeValue()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.getChildren();
+      fail("Text nodes shouldn't be able to use getChildren()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.getTagName();
+      fail("Text nodes shouldn't be able to use getTagName()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.hasAttribute("foo");
+      fail("Text nodes shouldn't be able to use hasAttribute()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.insertBefore(null, null);
+      fail("Text nodes shouldn't be able to use insertBefore()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.removeAttribute("foo");
+      fail("Text nodes shouldn't be able to use removeAttribute()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.removeChild(null);
+      fail("Text nodes shouldn't be able to use removeChild()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.setAttribute("id", "foo");
+      fail("Text nodes shouldn't be able to use setAttribute()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+    
+    try {
+      textNode.setTagName("div");
+      fail("Text nodes shouldn't be able to use setTagName()");
+    } catch (UnsupportedOperationException e) {
+      // Expected condition
+    }
+  }
+}
