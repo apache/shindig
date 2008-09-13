53755b53bae0c7e031cd6fd9ab5ff461c11490a3
Chris Chabot <chabotc@apache.org>
2008-09-13 10:17:17 +0000

    Code layout restructuring step1 : mv social-api social
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@694914 13f79535-47bb-0310-9956-ffa450edef68

--- php/test/ShindigAllTests.php
+++ php/test/ShindigAllTests.php
@@ -30,10 +30,10 @@ function __autoload($className)
 	$basePath = realpath('./php');
 	$locations = array('src/common', 'src/common/samplecontainer', 'src/gadgets', 
 			'src/gadgets/http', 'src/gadgets/oauth', 'src/gadgets/samplecontainer', 
-			'src/social-api', 'src/social-api/http', 'src/social-api/canonical', 
-			'src/social-api/dataservice', 'src/social-api/converters', 
-			'src/social-api/opensocial', 'src/social-api/opensocial/model', 
-			'src/social-api/samplecontainer');
+			'src/social', 'src/social/http', 'src/social/canonical', 
+			'src/social/dataservice', 'src/social/converters', 
+			'src/social/opensocial', 'src/social/opensocial/model', 
+			'src/social/samplecontainer');
 	$extension_class_paths = Config::get('extension_class_paths');
 	if (! empty($extension_class_paths)) {
 		$locations = array_merge(explode(',', $extension_class_paths), $locations);
@@ -69,7 +69,7 @@ class ShindigAllTests {
 		$suite = new PHPUnit_Framework_TestSuite();
 		$suite->setName('Shindig');
 		$path = realpath('./php/test/');
-		$testTypes = array('common', 'gadgets', 'social-api');
+		$testTypes = array('common', 'gadgets', 'social');
 		foreach ($testTypes as $type) {
 			foreach (glob("$path/{$type}/*Test.php") as $file) {
 				if (is_readable($file)) {
