2c479b0dd1111cbe48697b9723f7b8de2aef6a2b
Ian Boston <ieb@apache.org>
2008-09-19 08:52:05 +0000

    Clarified implementation of the PersonService which was not honoring the group resolution correctly. Implemented AppDataService in the process moving shared functionality into util classes. Started implementation of Activity service.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@696991 13f79535-47bb-0310-9956-ffa450edef68

--- java/samples/src/main/java/org/apache/shindig/social/opensocial/jpa/spi/ActivityServiceDb.java
+++ java/samples/src/main/java/org/apache/shindig/social/opensocial/jpa/spi/ActivityServiceDb.java
@@ -18,6 +18,10 @@
 package org.apache.shindig.social.opensocial.jpa.spi;
 
 import org.apache.shindig.auth.SecurityToken;
+import org.apache.shindig.common.util.ImmediateFuture;
+import org.apache.shindig.social.ResponseError;
+import org.apache.shindig.social.opensocial.jpa.ActivityDb;
+import org.apache.shindig.social.opensocial.jpa.ApplicationDataMapDb;
 import org.apache.shindig.social.opensocial.model.Activity;
 import org.apache.shindig.social.opensocial.spi.ActivityService;
 import org.apache.shindig.social.opensocial.spi.GroupId;
@@ -25,6 +29,13 @@ import org.apache.shindig.social.opensocial.spi.RestfulCollection;
 import org.apache.shindig.social.opensocial.spi.SocialSpiException;
 import org.apache.shindig.social.opensocial.spi.UserId;
 
+import com.google.inject.Inject;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Future;
 
@@ -32,6 +43,13 @@ import java.util.concurrent.Future;
  *
  */
 public class ActivityServiceDb implements ActivityService {
+
+  private EntityManager entityManager;
+
+  @Inject
+  public ActivityServiceDb(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
 
   /* (non-Javadoc)
    * @see org.apache.shindig.social.opensocial.spi.ActivityService#createActivity(org.apache.shindig.social.opensocial.spi.UserId, org.apache.shindig.social.opensocial.spi.GroupId, java.lang.String, java.util.Set, org.apache.shindig.social.opensocial.model.Activity, org.apache.shindig.auth.SecurityToken)
@@ -66,8 +84,7 @@ public class ActivityServiceDb implements ActivityService {
   public Future<RestfulCollection<Activity>> getActivities(UserId userId, GroupId groupId,
       String appId, Set<String> fields, Set<String> activityIds, SecurityToken token)
       throws SocialSpiException {
-    // TODO Auto-generated method stub
-    return null;
+    return ImmediateFuture.newInstance(new RestfulCollection<Activity>(getActivities(userId, activityIds, token)));
   }
 
   /* (non-Javadoc)
@@ -75,8 +92,56 @@ public class ActivityServiceDb implements ActivityService {
    */
   public Future<Activity> getActivity(UserId userId, GroupId groupId, String appId,
       Set<String> fields, String activityId, SecurityToken token) throws SocialSpiException {
-    // TODO Auto-generated method stub
+    Activity activity = getActivities(userId, activityId,  token);
+    if ( activity != null  ) {
+      return ImmediateFuture.newInstance(activity);
+    }
+    throw new SocialSpiException(ResponseError.BAD_REQUEST,"Cant find activity");
+  }
+
+  
+  /**
+   * @param userId
+   * @param groupId
+   * @param appId
+   * @param token
+   * @return
+   */
+  private Activity getActivities(UserId userId, String activityId,
+      SecurityToken token) {
+    Query q = entityManager.createNamedQuery(ActivityDb.FINDBY_ACTIVITY_ID);
+    String uid = SPIUtils.getUserList(userId, token);
+    q.setParameter(ActivityDb.PARAM_USERID, uid);
+    q.setParameter(ActivityDb.PARAM_ACTIVITYID, activityId);
+    q.setFirstResult(1);
+    q.setMaxResults(1);
+    List<?> activities = q.getResultList();
+    if ( activities != null && activities.size() > 0 ) {
+      return (Activity) activities.get(1);
+    }
     return null;
+  }
+
+
+  /**
+   * @param userId
+   * @param groupId
+   * @param appId
+   * @param token
+   * @return
+   */
+  private List<Activity> getActivities(UserId userId, Set<String> activityIds,
+      SecurityToken token) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(ActivityDb.JPQL_FINDBY_ACTIVITIES);
+    List<String> paramList = SPIUtils.toList(activityIds);
+    String uid = SPIUtils.getUserList(userId, token);
+    int lastPos = JPQLUtils.addInClause(sb, "a", "id", 1, paramList.size());
+    sb.append(" and a.userid = ?").append(lastPos);
+    lastPos++;
+    paramList.add(uid);
+    List<Activity> a = JPQLUtils.getListQuery(entityManager, sb.toString(), paramList, null);
+    return a;
   }
 
 }
