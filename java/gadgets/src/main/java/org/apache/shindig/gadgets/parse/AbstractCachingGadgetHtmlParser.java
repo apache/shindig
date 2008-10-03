1f184c5010018e323d002825f3eb3931a9511b03
John Hjelmstad <johnh@apache.org>
2008-10-03 00:23:28 +0000

    Adding infrastructure for automatic caching by GadgetHtmlParser implementations. This does not yet affect any existing operation, as it's not included in CajaHtmlParser.
    Two classes are provided:
    1. AbstractCachingGadgetHtmlParser. A GadgetHtmlParser implementation simply subclasses this with proper configuration (a cache and a minimum amount of time at which it's worth parsing parsed content) to automatically support caching. Content is cached by MD5 of the String inputs.
    
    2. ParseTreeSerializer, with Test. PTS handles conversion between byte[] <-> List<ParsedHtmlNode>. It implements custom serialization routines rather than using Java's built-in serialization for several reasons:
      A) Smaller storage requirements. Testing yielded between 10 and 30% space savings compared to Java serialization, with higher savings correlated with more-structured (fewer Strings) input.
      B) Faster execution time. Custom serialization measured between 30-40% faster than Java serialization, while custom deserialization measured 35-50% faster. For a "typical" sample gadget of ~30kB of content, serialization averaged 2.1ms and deserialization clocked in at an avg of 1.3ms. These compare quite favorably to the CajaHtmlParser's average parsing time of 25ms.
      C) Cross-platform, cross-parser, and easier cross-version (de)serialization. This rationale is weaker but noted for completeness.
    
    Usage of these classes, ideally combined with additional rules preventing hangman variable substitution in non-Text content (Text nodes and Attributes), should allow Shindig to obtain and process a gadget as a parse tree relatively cheaply.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@701267 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/parse/AbstractCachingGadgetHtmlParser.java
@@ -0,0 +1,83 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.apache.shindig.gadgets.parse;
+
+import org.apache.shindig.common.cache.Cache;
+import org.apache.shindig.common.cache.CacheProvider;
+import org.apache.shindig.common.util.HashUtil;
+import org.apache.shindig.gadgets.GadgetException;
+
+import java.util.List;
+
+/**
+ * Abstract base class for {@code GadgetHtmlParser} classes that provides a caching
+ * layer for parse trees. The cache is provided by a given {@code CacheProvider},
+ * with the configured size. The class is also configured with the minimum parse
+ * time, in milliseconds, for which it's worth caching contents at all. This
+ * is a rough heuristic, but useful to avoid caching contents that are parsed faster
+ * than a cache hit and serialization would take. A value <= 0 turns this feature off.
+ * 
+ * Essentially any real {@code GadgetHtmlParser} should extend this base class, as
+ * its abstract method's signature is identical to the interface.
+ */
+public abstract class AbstractCachingGadgetHtmlParser implements GadgetHtmlParser {
+  protected abstract List<ParsedHtmlNode> doParse(String source) throws GadgetException;
+  
+  private final Cache<String, byte[]> parseTreeCache;
+  private final ParseTreeSerializer pts;
+  private final int cacheTimeMsCutoff;
+  
+  protected AbstractCachingGadgetHtmlParser(CacheProvider cacheProvider, 
+      int capacity, int cacheTimeMsCutoff) {
+    if (cacheProvider != null && capacity > 0) {
+      parseTreeCache = cacheProvider.createCache(capacity);
+    } else {
+      // Cache can be configured to do nothing for test instances, etc.
+      parseTreeCache = new DoNothingCache();
+    }
+    pts = new ParseTreeSerializer();
+    this.cacheTimeMsCutoff = cacheTimeMsCutoff;
+  }
+
+  public List<ParsedHtmlNode> parse(String source) throws GadgetException {
+    // Cache key is MD5 of String
+    String cacheKey = HashUtil.checksum(source.getBytes());
+    byte[] cached = parseTreeCache.getElement(cacheKey);
+    if (cached != null) {
+      return pts.deserialize(cached);
+    }
+    
+    long parseStart = System.currentTimeMillis();
+    List<ParsedHtmlNode> parsed = doParse(source);
+    if (parsed == null) {
+      return null;
+    }
+    
+    if ((System.currentTimeMillis() - parseStart) > cacheTimeMsCutoff) {
+      parseTreeCache.addElement(cacheKey, pts.serialize(parsed));
+    }
+    
+    return parsed;
+  }
+  
+  private static class DoNothingCache implements Cache<String, byte[]> {
+    public void addElement(String key, byte[] value) { }
+    public byte[] getElement(String key) { return null; }
+    public byte[] removeElement(String key) { return null; }
+  }
+}
