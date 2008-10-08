08e2425884152c94dd9db0c36dc475fb643489c3
John Hjelmstad <johnh@apache.org>
2008-10-08 23:36:02 +0000

    Adding support for JSONP calling of /gadgets/metadata. This closes issue SHINDIG-624.
    Many thanks to Michael Hermanto for the patch!
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@703022 13f79535-47bb-0310-9956-ffa450edef68

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/servlet/RpcServlet.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/servlet/RpcServlet.java
@@ -18,48 +18,77 @@
  */
 package org.apache.shindig.gadgets.servlet;
 
-import org.apache.shindig.common.servlet.InjectedServlet;
-
 import com.google.inject.Inject;
-
 import org.apache.commons.io.IOUtils;
+import org.apache.shindig.common.servlet.InjectedServlet;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 /**
  * Handles RPC metadata requests.
  */
 public class RpcServlet extends InjectedServlet {
-  private static final int MAX_REQUEST_SIZE = 1024 * 128;
-  private static final Logger logger
-      = Logger.getLogger("org.apache.shindig.gadgets");
+  static final String GET_REQUEST_REQ_PARAM = "req";
+  static final String GET_REQUEST_CALLBACK_PARAM = "callback";
+  static final Pattern GET_REQUEST_CALLBACK_PATTERN = Pattern.compile("[A-Za-z_\\.]+");
+
+  private static final int POST_REQUEST_MAX_SIZE = 1024 * 128;
+  private static final Logger logger = Logger.getLogger("org.apache.shindig.gadgets");
 
   private JsonRpcHandler jsonHandler;
+
   @Inject
   public void setJsonRpcHandler(JsonRpcHandler jsonHandler) {
     this.jsonHandler = jsonHandler;
   }
 
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    String reqValue;
+    String callbackValue;
+
+    try {
+      reqValue = validateParameterValue(request, GET_REQUEST_REQ_PARAM);
+      callbackValue = validateParameterValue(request, GET_REQUEST_CALLBACK_PARAM);
+      if (!GET_REQUEST_CALLBACK_PATTERN.matcher(callbackValue).matches()) {
+        throw new IllegalArgumentException("Wrong format for parameter '" +
+            GET_REQUEST_CALLBACK_PARAM + "' specified. Expected: " +
+            GET_REQUEST_CALLBACK_PATTERN.toString());
+      }
+
+    } catch (IllegalArgumentException e) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      logger.log(Level.INFO, e.getMessage(), e);
+      return;
+    }
+
+    Result result = process(request, response, reqValue.getBytes());
+    response.getWriter().write(result.isSuccess()
+        ? callbackValue + "(" + result.getOutput() + ")"
+        : result.getOutput());
+  }
 
   @Override
-  protected void doPost(HttpServletRequest request,
-      HttpServletResponse response) throws IOException {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
     int length = request.getContentLength();
     if (length <= 0) {
       logger.info("No Content-Length specified.");
       response.setStatus(HttpServletResponse.SC_LENGTH_REQUIRED);
       return;
     }
-    if (length > MAX_REQUEST_SIZE) {
+    if (length > POST_REQUEST_MAX_SIZE) {
       logger.info("Request size too large: " + length);
       response.setStatus(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE);
       return;
@@ -73,29 +102,66 @@ public class RpcServlet extends InjectedServlet {
       return;
     }
 
+    Result result = process(request, response, body);
+    response.getWriter().write(result.getOutput());
+  }
+  
+  private String validateParameterValue(HttpServletRequest request, String parameter)
+      throws IllegalArgumentException {
+    String result = request.getParameter(parameter);
+    if (result == null) {
+      throw new IllegalArgumentException("No parameter '" + parameter + "' specified.");
+    }
+    return result;
+  }
+  
+  private Result process(HttpServletRequest request, HttpServletResponse response, byte[] body) {
     try {
-      String encoding = request.getCharacterEncoding();
-      if (encoding == null) {
-        encoding = "UTF-8";
-      }
+      String encoding = getRequestCharacterEncoding(request);
       JSONObject req = new JSONObject(new String(body, encoding));
-
       JSONObject resp = jsonHandler.process(req);
       response.setStatus(HttpServletResponse.SC_OK);
       response.setContentType("application/json; charset=utf-8");
       response.setHeader("Content-Disposition", "attachment;filename=rpc.txt");
-      response.getWriter().write(resp.toString());
+      return new Result(resp.toString(), true);
     } catch (UnsupportedEncodingException e) {
       response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      response.getWriter().write("Unsupported input character set");
       logger.log(Level.INFO, e.getMessage(), e);
+      return new Result("Unsupported input character set", false);
     } catch (JSONException e) {
       response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      response.getWriter().write("Malformed JSON request.");
+      return new Result("Malformed JSON request.", false);
     } catch (RpcException e) {
       response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-      response.getWriter().write(e.getMessage());
       logger.log(Level.INFO, e.getMessage(), e);
+      return new Result(e.getMessage(), false);
     }
   }
+  
+  private String getRequestCharacterEncoding(HttpServletRequest request) {
+    String encoding = request.getCharacterEncoding();
+    if (encoding == null) {
+      encoding = "UTF-8";
+    }
+    return encoding;
+  }
+
+  private static class Result {
+    private final String output;
+    private final boolean success;
+
+    public Result(String output, boolean success) {
+      this.output = output;
+      this.success = success;
+    }
+
+    public String getOutput() {
+      return output;
+    }
+
+    public boolean isSuccess() {
+      return success;
+    }
+  }
+
 }
