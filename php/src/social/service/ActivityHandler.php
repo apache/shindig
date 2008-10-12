e0f3600aac8a3a6cee1916981126d9bc15a11a17
Chris Chabot <chabotc@apache.org>
2008-10-12 17:31:25 +0000

    Fixed and verified activity creation via REST, also fixed up the Activity SPI, some params and collection fields were missing
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@703852 13f79535-47bb-0310-9956-ffa450edef68

--- php/src/social/service/ActivityHandler.php
+++ php/src/social/service/ActivityHandler.php
@@ -65,13 +65,10 @@ class ActivityHandler extends DataRequestHandler {
 			if (count($optionalActivityIds) == 1) {
 				return $this->service->getActivity($userIds[0], $requestItem->getGroup(), $requestItem->getAppId(), $requestItem->getFields(), $optionalActivityIds[0], $requestItem->getToken());
 			} else {
-				return $this->service->getActivities($userIds[0], $requestItem->getGroup(), $requestItem->getAppId(), $requestItem->getFields(), $optionalActivityIds, $requestItem->getToken());
+				return $this->service->getActivities($userIds[0], $requestItem->getGroup(), $requestItem->getAppId(), $requestItem->getSortBy(), $requestItem->getFilterBy(), $requestItem->getStartIndex(), $requestItem->getCount(), $requestItem->getFields(), $optionalActivityIds, $requestItem->getToken());
 			}
 		}
-		return $this->service->getActivities($userIds, $requestItem->getGroup(), $requestItem->getAppId(), 
-			// TODO: add pagination and sorting support
-			// getSortBy(params), getFilterBy(params), getStartIndex(params), getCount(params),
-			$requestItem->getFields(), $requestItem->getToken());
+		return $this->service->getActivities($userIds, $requestItem->getGroup(), $requestItem->getAppId(), $requestItem->getSortBy(), $requestItem->getFilterBy(), $requestItem->getStartIndex(), $requestItem->getCount(), $requestItem->getFields(), $requestItem->getToken());
 	}
 
 	/**
