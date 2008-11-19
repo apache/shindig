80553d96fd529a73b5fe84fbe11791814e45cf91
Chris Chabot <chabotc@apache.org>
2008-11-19 08:54:03 +0000

    SHINDIG-688 by Nagy Attila - SigningFetcher drops port specification in url
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@718909 13f79535-47bb-0310-9956-ffa450edef68

--- php/src/gadgets/SigningFetcher.php
+++ php/src/gadgets/SigningFetcher.php
@@ -1,16 +1,21 @@
 <?php
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 /**
@@ -175,7 +180,7 @@ class SigningFetcher extends RemoteContentFetcher {
 			// Careful here; the OAuth form encoding scheme is slightly different than
 			// the normal form encoding scheme, so we have to use the OAuth library
 			// formEncode method.
-			$url = $parsedUri['scheme'] . '://' . $parsedUri['host'] . $parsedUri['path'] . '?' . $newQuery;
+			$url = $parsedUri['scheme'] . '://' . $parsedUri['host'] . (isset($parsedUri['port']) ? ':' . $parsedUri['port'] : '') . $parsedUri['path'] . '?' . $newQuery;
 			// The headers are transmitted in the POST-data array in the field 'headers'
 			// if no post should be made, the value should be false for this parameter
 			$postHeaders = ((isset($_POST['headers']) && $method == 'POST') ? $_POST['headers'] : false);
