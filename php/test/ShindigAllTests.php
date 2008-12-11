9ae51a7c25c04e94f81b181df049d65e1b98b19b
Chris Chabot <chabotc@apache.org>
2008-12-11 00:37:37 +0000

    Sync up with fixes on trunk
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/branches/1.0.x-incubating@725500 13f79535-47bb-0310-9956-ffa450edef68

--- php/test/ShindigAllTests.php
+++ php/test/ShindigAllTests.php
@@ -47,7 +47,7 @@ function __autoload($className) {
 
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath('./php') . PATH_SEPARATOR . realpath('./php/external'));
 ini_set('error_reporting', E_COMPILE_ERROR | E_ERROR | E_CORE_ERROR);
-require_once 'config.php';
+require_once 'src/common/Config.php';
 require_once 'test/TestContext.php';
 
 if (defined('PHPUnit_MAIN_METHOD') === false) {
