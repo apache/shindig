48eb6a62210a8a3ea5c2175b628fcdd56cc1b4b0
Paul Lindner <lindner@apache.org>
2009-01-12 09:16:52 +0000

    fix package name
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@733657 13f79535-47bb-0310-9956-ffa450edef68

--- java/common/src/test/java/org/apache/shindig/common/AllTests.java
+++ java/common/src/test/java/org/apache/shindig/common/AllTests.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.apache.shindig.gadgets;
+package org.apache.shindig.common;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
