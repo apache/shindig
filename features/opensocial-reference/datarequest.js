25efe483ef672d65046f463a9a020fe43e0240c5
Paul Lindner <lindner@apache.org>
2009-01-23 01:42:50 +0000

    SHINDIG-879 | remove extra comment
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@736885 13f79535-47bb-0310-9956-ffa450edef68

--- features/opensocial-reference/datarequest.js
+++ features/opensocial-reference/datarequest.js
@@ -489,7 +489,7 @@ opensocial.DataRequest.prototype.newFetchActivitiesRequest = function(idSpec,
     opt_params) {
   opt_params = opt_params || {};
 
-  var fields = opensocial.DataRequest.PeopleRequestFields; // THIS LINE IS MISSING 
+  var fields = opensocial.DataRequest.PeopleRequestFields;
 
   this.addDefaultParam(opt_params, fields.FIRST, 0);
   this.addDefaultParam(opt_params, fields.MAX, 20);
