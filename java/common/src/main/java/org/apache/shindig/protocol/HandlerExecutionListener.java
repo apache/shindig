bf0aaf2b8b58db5691b7ba0aae23628a9c9693bb
Paul Lindner <lindner@apache.org>
2009-03-23 18:09:18 +0000

    SHINDIG-990 | Add executed lifecycle phase to allow for profiling implementations, add sample logging implementation/docs
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/shindig/trunk@757479 13f79535-47bb-0310-9956-ffa450edef68

--- java/common/src/main/java/org/apache/shindig/protocol/HandlerExecutionListener.java
+++ java/common/src/main/java/org/apache/shindig/protocol/HandlerExecutionListener.java
@@ -20,11 +20,13 @@ package org.apache.shindig.protocol;
 
 import com.google.inject.ImplementedBy;
 
+import java.util.logging.Logger;
+
 /**
  * Called by the handler dispatcher prior to executing a handler. Used to allow
  * containers to implement cross-cutting features such as request logging.
  */
-@ImplementedBy(HandlerExecutionListener.NoOpHandlerExecutionListener.class)
+@ImplementedBy(HandlerExecutionListener.NoOpHandler.class)
 public interface HandlerExecutionListener {
 
   /**
@@ -34,14 +36,36 @@ public interface HandlerExecutionListener {
    * @param request being executed
    */
   void executing(String service, String operation, RequestItem request);
+  void executed(String service, String operation, RequestItem request);
 
   /**
    * Default no-op implementation
    */
-  public static class NoOpHandlerExecutionListener implements HandlerExecutionListener {
+  public static class NoOpHandler implements HandlerExecutionListener {
 
     public void executing(String service, String operation, RequestItem request) {
       // No-op
+    }
+    public void executed(String service, String operation, RequestItem request) {
+      // No-op
+    }
+  }
+
+  /**
+   * A simple implementation that logs the start/stop times of requests
+   *
+   * You can configure this for use by adding a binding in your Guice Module like this:
+   *   bind(HandlerExecutionListener.class).to(HandlerExecutionListener.LoggingHandler.class);
+   */
+  
+  public static class LoggingHandler implements HandlerExecutionListener {
+    public static final Logger LOG = Logger.getLogger(HandlerExecutionListener.class.toString());
+
+    public void executing(String service, String operation, RequestItem request) {
+      LOG.info("start - " + service + " " + operation);
+    }
+    public void executed(String service, String operation, RequestItem request) {
+      LOG.info("  end - " + service + " " + operation);
     }
   }
 }
