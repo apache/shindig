85bcfad9c80f54093ba4cbef753bd70965a3fac9
Louis Ryan <lryan@apache.org>
2009-05-01 18:34:02 +0000

    Make RpcServiceLookup an interface.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shindig/trunk@770784 13f79535-47bb-0310-9956-ffa450edef68

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/render/RpcServiceLookup.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/render/RpcServiceLookup.java
@@ -1,95 +1,47 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * with the License. You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.apache.shindig.gadgets.render;
 
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.TimeUnit;
-
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.MapMaker;
 import com.google.common.collect.Multimap;
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
-import com.google.inject.name.Named;
+import com.google.inject.ImplementedBy;
 
 /**
- * Simple storage for holding the various services offered by containers.
- *
- * This storage is keyed by container, and within each container, the services are stored, keyed by,
- * the endpoint url.
- *
- * Here is a json structure that shows how data is stored:
- *
- * { container1 : { "http://.../endpoint1" : ["system.listMethods", "people.get", "people.create",
- * "people.delete"], ... }, "http://.../endpoint2" : { "system.listMethods", "cache.invalidate"],
- * ... } }, container 2 : ..... }
+ * Provide information about the set of JSON-RPC services that are available to gadgets running in
+ * the context of a specific container
  */
-@Singleton
-public class RpcServiceLookup {
-
-  private final ConcurrentMap<String, Multimap<String, String>> containerServices;
-
-  private final RpcServiceFetcher fetcher;
-
+@ImplementedBy(DefaultRpcServiceLookup.class)
+public interface RpcServiceLookup {
   /**
-   * @param fetcher  RpcServiceFetcher to retrieve services available from endpoints
-   * @param duration in seconds service definitions should remain in the cache
-   */
-  @Inject
-  public RpcServiceLookup(RpcServiceFetcher fetcher,
-      @Named("org.apache.shindig.serviceExpirationDurationMinutes")Long duration) {
-    containerServices = new MapMaker().expiration(duration * 60, TimeUnit.SECONDS).makeMap();
-    this.fetcher = fetcher;
-  }
-
-  /**
-   * @param container Syndicator param identifying the container for whom we want services
-   * @param host      Host for which gadget is being rendered, used to do substitution in endpoints
-   * @return Map of Services, by endpoint for the given container.
-   */
-  public Multimap<String, String> getServicesFor(String container, String host) {
-    Preconditions.checkNotNull(container);
-    Preconditions.checkArgument(container.length() != 0);
-    Preconditions.checkNotNull(host);
-
-    Multimap<String, String> foundServices = containerServices.get(container);
-    if (foundServices == null) {
-      foundServices = fetcher.getServicesForContainer(container, host);
-      if (foundServices != null) {
-        setServicesFor(container, foundServices);
-      }
-    }
-    if (foundServices == null) {
-      foundServices = ImmutableMultimap.<String, String>builder().build();
-    }
-    return foundServices;
-  }
-
-  /**
-   * Setup the services for a given container.
+   * This result map is a map of the form
+   * { endpoint1 -> [services]}, endpoint2 -> [services]}
    *
-   * @param container     The param identifying this container.
-   * @param foundServices Map of services, keyed by endpoint.
+   * Services are described using the names expected as a result of JSON-RPC call
+   * to system.listMethods
+   *
+   * When a gadget is rendered the container data is mapped into gadgets.config and used to
+   * initialize osapi, which typically results in output that looks like
+   *
+   * { "osapi.services" : { "http://.../endpoint1" : ["system.listMethods", "people.get", "people.create",
+   * "people.delete"], ... }, "http://.../endpoint2" : { "system.listMethods", "cache.invalidate"],
+   * ... } }}
+   * @param container
+   * @param host
+   * @return
    */
-  void setServicesFor(String container, Multimap<String, String> foundServices) {
-    containerServices.put(container, foundServices);
-  }
-
+  Multimap<String, String> getServicesFor(String container, String host);
 }
