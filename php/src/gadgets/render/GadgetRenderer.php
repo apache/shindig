becb7100b9d8919cd0cf728de7d855be2dcb9e9f
Chris Chabot <chabotc@apache.org>
2010-02-10 08:27:34 +0000

    Patch by Jacky Wang - Don't expand feartures in forcedJsLibs script tag
    git-svn-id: https://svn.apache.org/repos/asf/shindig/trunk@908384 13f79535-47bb-0310-9956-ffa450edef68

--- php/src/gadgets/render/GadgetRenderer.php
+++ php/src/gadgets/render/GadgetRenderer.php
@@ -33,7 +33,7 @@ abstract class GadgetRenderer {
    * generates the library string (core:caja:etc.js) including a checksum of all the
    * javascript content (?v=<md5 of js>) for cache busting
    *
-   * @param string $features
+   * @param array $features
    * @param Gadget $gadget
    * @return string the list of libraries in core:caja:etc.js?v=checksum> format
    */
@@ -41,10 +41,17 @@ abstract class GadgetRenderer {
     if (! is_array($features) || ! count($features)) {
       return 'null';
     }
-    $ret = implode(':', $features);
+    $registry = $this->context->getRegistry();
+    // Given the JsServlet automatically expends the js library, we just need
+    // to include the "leaf" nodes of the features.
+    $ret = $features;
+    foreach ($features as $feature) {
+      $depFeatures = $registry->features[$feature]['deps'];
+      $ret = array_diff($ret, $depFeatures);
+    }
+    $ret = implode(':', $ret);
     $cache = Cache::createCache(Config::get('feature_cache'), 'FeatureCache');
     if (($md5 = $cache->get(md5('getJsUrlMD5'))) === false) {
-      $registry = $this->context->getRegistry();
       $features = $registry->features;
 
       // Build a version string from the md5() checksum of all included javascript
