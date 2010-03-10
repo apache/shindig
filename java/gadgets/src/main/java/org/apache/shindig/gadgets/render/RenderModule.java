376ec8a28719f116b001c9a6d1e59b496c883ded
John Hjelmstad <johnh@apache.org>
2010-03-10 23:31:29 +0000

    Provide dual bindings for old and new SanitizingGadgetRewriter during transition time.
    git-svn-id: https://svn.apache.org/repos/asf/shindig/trunk@921625 13f79535-47bb-0310-9956-ffa450edef68

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/render/RenderModule.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/render/RenderModule.java
@@ -20,8 +20,6 @@ package org.apache.shindig.gadgets.render;
 
 import java.util.Set;
 
-import org.apache.shindig.gadgets.render.old.SanitizingGadgetRewriter;
-
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
@@ -36,24 +34,31 @@ public class RenderModule extends AbstractModule {
 
     // NOTE: Sanitization only works when using the "full" Neko HTML parser. It is not recommended
     // that you attempt to use sanitization without it.
+    Set<String> allowedTags = ImmutableSet.of("a", "abbr", "acronym", "area", "b", "bdo", "big", "blockquote",
+        "body", "br", "caption", "center", "cite", "code", "col", "colgroup", "dd", "del",
+        "dfn", "div", "dl", "dt", "em", "font", "h1", "h2", "h3", "h4", "h5", "h6", "head",
+        "hr", "html", "i", "img", "ins", "legend", "li", "link", "map", "ol", "p", "pre",
+        "q", "s", "samp", "small", "span", "strike", "strong", "style", "sub", "sup", "table",
+        "tbody", "td", "tfoot", "th", "thead", "tr", "tt", "u", "ul");
     bind(setLiteral)
-        .annotatedWith(SanitizingGadgetRewriter.AllowedTags.class)
-        .toInstance(ImmutableSet.of("a", "abbr", "acronym", "area", "b", "bdo", "big", "blockquote",
-            "body", "br", "caption", "center", "cite", "code", "col", "colgroup", "dd", "del",
-            "dfn", "div", "dl", "dt", "em", "font", "h1", "h2", "h3", "h4", "h5", "h6", "head",
-            "hr", "html", "i", "img", "ins", "legend", "li", "link", "map", "ol", "p", "pre",
-            "q", "s", "samp", "small", "span", "strike", "strong", "style", "sub", "sup", "table",
-            "tbody", "td", "tfoot", "th", "thead", "tr", "tt", "u", "ul"));
-
+        .annotatedWith(org.apache.shindig.gadgets.render.old.SanitizingGadgetRewriter.AllowedTags.class)
+        .toInstance(allowedTags);
     bind(setLiteral)
-        .annotatedWith(SanitizingGadgetRewriter.AllowedAttributes.class)
-        .toInstance(ImmutableSet.of("abbr", "align", "alt", "axis", "bgcolor", "border",
-            "cellpadding", "cellspacing", "char", "charoff", "cite", "class", "clear", "color",
-            "cols", "colspan", "compact", "coords", "datetime", "dir", "face", "headers", "height",
-            "href", "hreflang", "hspace", "id", "ismap", "lang", "longdesc", "name", "nohref",
-            "noshade", "nowrap", "rel", "rev", "rowspan", "rules", "scope", "shape", "size", "span",
-            "src", "start", "style", "summary", "title", "type", "usemap", "valign", "value",
-            "vspace", "width"));
+        .annotatedWith(org.apache.shindig.gadgets.render.SanitizingGadgetRewriter.AllowedTags.class)
+        .toInstance(allowedTags);
 
+    Set<String> allowedAttributes = ImmutableSet.of("abbr", "align", "alt", "axis", "bgcolor", "border",
+        "cellpadding", "cellspacing", "char", "charoff", "cite", "class", "clear", "color",
+        "cols", "colspan", "compact", "coords", "datetime", "dir", "face", "headers", "height",
+        "href", "hreflang", "hspace", "id", "ismap", "lang", "longdesc", "name", "nohref",
+        "noshade", "nowrap", "rel", "rev", "rowspan", "rules", "scope", "shape", "size", "span",
+        "src", "start", "style", "summary", "title", "type", "usemap", "valign", "value",
+        "vspace", "width");
+    bind(setLiteral)
+        .annotatedWith(org.apache.shindig.gadgets.render.old.SanitizingGadgetRewriter.AllowedAttributes.class)
+        .toInstance(allowedAttributes);
+    bind(setLiteral)
+        .annotatedWith(org.apache.shindig.gadgets.render.SanitizingGadgetRewriter.AllowedAttributes.class)
+        .toInstance(allowedAttributes);
   }
 }
