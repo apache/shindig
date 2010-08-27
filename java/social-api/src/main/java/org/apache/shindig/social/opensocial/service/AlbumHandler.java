c7c89e281ea5de9929bc3fe13d09a2132f210dc6
Paul Lindner <lindner@apache.org>
2010-08-27 12:00:58 +0000

    SHINDIG-1410 | Spacing fixes
    git-svn-id: https://svn.apache.org/repos/asf/shindig/trunk@990116 13f79535-47bb-0310-9956-ffa450edef68

--- java/social-api/src/main/java/org/apache/shindig/social/opensocial/service/AlbumHandler.java
+++ java/social-api/src/main/java/org/apache/shindig/social/opensocial/service/AlbumHandler.java
@@ -45,141 +45,141 @@ import com.google.inject.Inject;
 @Service(name = "albums", path = "/{userId}+/{groupId}/{albumId}+")
 public class AlbumHandler {
 
-	private final AlbumService service;
-	private final ContainerConfig config;
+  private final AlbumService service;
+  private final ContainerConfig config;
 
-	@Inject
-	public AlbumHandler(AlbumService service, ContainerConfig config) {
-		this.service = service;
-		this.config = config;
-	}
+  @Inject
+  public AlbumHandler(AlbumService service, ContainerConfig config) {
+    this.service = service;
+    this.config = config;
+  }
 
-	/*
-	 * Handles create operations.
-	 * 
-	 * Allowed end-points: /albums/{userId}/@self
-	 * 
-	 * Examples: /albums/john.doe/@self
-	 */
-	@Operation(httpMethods = "POST", bodyParam = "album")
-	public Future<?> create(SocialRequestItem request) throws ProtocolException {
-		// Retrieve userIds and albumIds
-		Set<UserId> userIds = request.getUsers();
-		List<String> albumIds = request.getListParameter("albumId");
+  /*
+    * Handles create operations.
+    *
+    * Allowed end-points: /albums/{userId}/@self
+    *
+    * Examples: /albums/john.doe/@self
+    */
+  @Operation(httpMethods = "POST", bodyParam = "album")
+  public Future<?> create(SocialRequestItem request) throws ProtocolException {
+    // Retrieve userIds and albumIds
+    Set<UserId> userIds = request.getUsers();
+    List<String> albumIds = request.getListParameter("albumId");
 
-		// Preconditions - exactly one userId specified, no albumIds specified
-		HandlerPreconditions.requireNotEmpty(userIds, "No userId specified");
-		HandlerPreconditions.requireSingular(userIds, "Multiple userIds not supported");
-		HandlerPreconditions.requireEmpty(albumIds, "Cannot specify albumId in create");
+    // Preconditions - exactly one userId specified, no albumIds specified
+    HandlerPreconditions.requireNotEmpty(userIds, "No userId specified");
+    HandlerPreconditions.requireSingular(userIds, "Multiple userIds not supported");
+    HandlerPreconditions.requireEmpty(albumIds, "Cannot specify albumId in create");
 
-		return service.createAlbum(Iterables.getOnlyElement(userIds),
-				request.getAppId(),
-				request.getTypedParameter("album", Album.class),
-				request.getToken());
-	}
+    return service.createAlbum(Iterables.getOnlyElement(userIds),
+        request.getAppId(),
+        request.getTypedParameter("album", Album.class),
+        request.getToken());
+  }
 
-	/*
-	 * Handles retrieve operations.
-	 * 
-	 * Allowed end-points: /albums/{userId}+/{groupId}/{albumId}+
-	 * 
-	 * Examples: /albums/@me/@self /albums/john.doe/@self/1,2
-	 * /albums/john.doe,jane.doe/@friends
-	 */
-	@Operation(httpMethods = "GET")
-	public Future<?> get(SocialRequestItem request) throws ProtocolException {
-		// Get user, group, and album IDs
-		Set<UserId> userIds = request.getUsers();
-		Set<String> optionalAlbumIds = ImmutableSet.copyOf(request
-				.getListParameter("albumId"));
+  /*
+    * Handles retrieve operations.
+    *
+    * Allowed end-points: /albums/{userId}+/{groupId}/{albumId}+
+    *
+    * Examples: /albums/@me/@self /albums/john.doe/@self/1,2
+    * /albums/john.doe,jane.doe/@friends
+    */
+  @Operation(httpMethods = "GET")
+  public Future<?> get(SocialRequestItem request) throws ProtocolException {
+    // Get user, group, and album IDs
+    Set<UserId> userIds = request.getUsers();
+    Set<String> optionalAlbumIds = ImmutableSet.copyOf(request
+        .getListParameter("albumId"));
 
-		// At least one userId must be specified
-		HandlerPreconditions.requireNotEmpty(userIds, "No userId specified");
+    // At least one userId must be specified
+    HandlerPreconditions.requireNotEmpty(userIds, "No userId specified");
 
-		// If multiple userIds specified, albumIds must not be specified
-		if (userIds.size() > 1 && !optionalAlbumIds.isEmpty()) {
-			throw new IllegalArgumentException("Cannot fetch same albumIds for multiple userIds");
-		}
+    // If multiple userIds specified, albumIds must not be specified
+    if (userIds.size() > 1 && !optionalAlbumIds.isEmpty()) {
+      throw new IllegalArgumentException("Cannot fetch same albumIds for multiple userIds");
+    }
 
-		// Retrieve albums by ID
-		if (!optionalAlbumIds.isEmpty()) {
-			if (optionalAlbumIds.size() == 1) {
-				return service.getAlbum(Iterables.getOnlyElement(userIds),
-						request.getAppId(), request.getFields(),
-						optionalAlbumIds.iterator().next(), request.getToken());
-			} else {
-				return service.getAlbums(Iterables.getOnlyElement(userIds),
-						request.getAppId(), request.getFields(),
-						new CollectionOptions(request), optionalAlbumIds,
-						request.getToken());
-			}
-		}
+    // Retrieve albums by ID
+    if (!optionalAlbumIds.isEmpty()) {
+      if (optionalAlbumIds.size() == 1) {
+        return service.getAlbum(Iterables.getOnlyElement(userIds),
+            request.getAppId(), request.getFields(),
+            optionalAlbumIds.iterator().next(), request.getToken());
+      } else {
+        return service.getAlbums(Iterables.getOnlyElement(userIds),
+            request.getAppId(), request.getFields(),
+            new CollectionOptions(request), optionalAlbumIds,
+            request.getToken());
+      }
+    }
 
-		// Retrieve albums by group
-		return service.getAlbums(userIds, request.getGroup(), request
-				.getAppId(), request.getFields(),
-				new CollectionOptions(request), request.getToken());
-	}
+    // Retrieve albums by group
+    return service.getAlbums(userIds, request.getGroup(), request
+        .getAppId(), request.getFields(),
+        new CollectionOptions(request), request.getToken());
+  }
 
-	/*
-	 * Handles update operations.
-	 * 
-	 * Allowed end-points: /albums/{userId}/@self/{albumId}
-	 * 
-	 * Examples: /albums/john.doe/@self/1
-	 */
-	@Operation(httpMethods = "PUT", bodyParam = "album")
-	public Future<?> update(SocialRequestItem request) throws ProtocolException {
-		// Retrieve userIds and albumIds
-		Set<UserId> userIds = request.getUsers();
-		List<String> albumIds = request.getListParameter("albumId");
+  /*
+    * Handles update operations.
+    *
+    * Allowed end-points: /albums/{userId}/@self/{albumId}
+    *
+    * Examples: /albums/john.doe/@self/1
+    */
+  @Operation(httpMethods = "PUT", bodyParam = "album")
+  public Future<?> update(SocialRequestItem request) throws ProtocolException {
+    // Retrieve userIds and albumIds
+    Set<UserId> userIds = request.getUsers();
+    List<String> albumIds = request.getListParameter("albumId");
 
-		// Enforce preconditions - exactly one user and one album specified
-		HandlerPreconditions.requireNotEmpty(userIds, "No userId specified");
-		HandlerPreconditions.requireSingular(userIds, "Multiple userIds not supported");
-		HandlerPreconditions.requireNotEmpty(albumIds, "No albumId specified");
-		HandlerPreconditions.requireSingular(albumIds, "Multiple albumIds not supported");
+    // Enforce preconditions - exactly one user and one album specified
+    HandlerPreconditions.requireNotEmpty(userIds, "No userId specified");
+    HandlerPreconditions.requireSingular(userIds, "Multiple userIds not supported");
+    HandlerPreconditions.requireNotEmpty(albumIds, "No albumId specified");
+    HandlerPreconditions.requireSingular(albumIds, "Multiple albumIds not supported");
 
-		return service.updateAlbum(Iterables.getOnlyElement(userIds),
-				request.getAppId(),
-				request.getTypedParameter("album", Album.class),
-				Iterables.getOnlyElement(albumIds), request.getToken());
-	}
+    return service.updateAlbum(Iterables.getOnlyElement(userIds),
+        request.getAppId(),
+        request.getTypedParameter("album", Album.class),
+        Iterables.getOnlyElement(albumIds), request.getToken());
+  }
 
-	/*
-	 * Handles delete operations.
-	 * 
-	 * Allowed end-points: /albums/{userId}/@self/{albumId}
-	 * 
-	 * Examples: /albums/john.doe/@self/1
-	 */
-	@Operation(httpMethods = "DELETE")
-	public Future<?> delete(SocialRequestItem request) throws ProtocolException {
-		// Get user and album ID
-		Set<UserId> userIds = request.getUsers();
-		String albumId = request.getParameter("albumId");
+  /*
+    * Handles delete operations.
+    *
+    * Allowed end-points: /albums/{userId}/@self/{albumId}
+    *
+    * Examples: /albums/john.doe/@self/1
+    */
+  @Operation(httpMethods = "DELETE")
+  public Future<?> delete(SocialRequestItem request) throws ProtocolException {
+    // Get user and album ID
+    Set<UserId> userIds = request.getUsers();
+    String albumId = request.getParameter("albumId");
 
-		// Enforce preconditions - userIds must contain exactly one element
-		HandlerPreconditions.requireNotEmpty(userIds, "No userId specified");
-		HandlerPreconditions.requireSingular(userIds, "Multiple userIds not supported");
+    // Enforce preconditions - userIds must contain exactly one element
+    HandlerPreconditions.requireNotEmpty(userIds, "No userId specified");
+    HandlerPreconditions.requireSingular(userIds, "Multiple userIds not supported");
 
-		// Service request
-		return service.deleteAlbum(Iterables.getOnlyElement(userIds),
-				request.getAppId(), albumId, request.getToken());
-	}
-	
-	/*
-	 * Retrieves supported fields for the albums service.
-	 */
-	@Operation(httpMethods = "GET", path = "/@supportedFields")
-	public List<Object> supportedFields(RequestItem request) {
-		String container = firstNonNull(request.getToken().getContainer(),
-				ContainerConfig.DEFAULT_CONTAINER);
-		return config.getList(container,
-				"${Cur['gadgets.features'].opensocial.supportedFields.album}");
-	}
+    // Service request
+    return service.deleteAlbum(Iterables.getOnlyElement(userIds),
+        request.getAppId(), albumId, request.getToken());
+  }
 
-	private static <T> T firstNonNull(T first, T second) {
-		return first != null ? first : Preconditions.checkNotNull(second);
-	}
+  /*
+    * Retrieves supported fields for the albums service.
+    */
+  @Operation(httpMethods = "GET", path = "/@supportedFields")
+  public List<Object> supportedFields(RequestItem request) {
+    String container = firstNonNull(request.getToken().getContainer(),
+        ContainerConfig.DEFAULT_CONTAINER);
+    return config.getList(container,
+        "${Cur['gadgets.features'].opensocial.supportedFields.album}");
+  }
+
+  private static <T> T firstNonNull(T first, T second) {
+    return first != null ? first : Preconditions.checkNotNull(second);
+  }
 }
