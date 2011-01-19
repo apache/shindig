af1a134c10138f757ef877b08d064b5981bf09d2
Han Thi Ngoc Nguyen <hnguy@apache.org>
2011-01-19 17:05:59 +0000

    SHINDIG-1490 | Added gadget views and few other features to the common test container | patch from Han Nguyen
    git-svn-id: https://svn.apache.org/repos/asf/shindig/trunk@1060867 13f79535-47bb-0310-9956-ffa450edef68

---
+++ content/samplecontainer/examples/commoncontainer/assembler.js
@@ -0,0 +1,114 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
+
+var testConfig =  testConfig || {};
+testConfig[shindig.container.ServiceConfig.API_PATH] = '/rpc';	
+testConfig[shindig.container.ContainerConfig.RENDER_DEBUG] = "1";
+
+//Default the security token for testing.
+shindig.auth.updateSecurityToken('john.doe:john.doe:appid:cont:url:0:default');
+
+
+//  Create the new CommonContainer 
+var CommonContainer = new shindig.container.Container(testConfig);
+
+// Need to pull these from values supplied in the dialog
+CommonContainer.init = function() {
+	//Create my new managed hub
+	CommonContainer.managedHub = new OpenAjax.hub.ManagedHub(
+	   {
+	   onSubscribe: function(topic, container) {
+        log(container.getClientID() + " subscribes to this topic '" + topic + "'");
+        return true;
+        // return false to reject the request.
+      },
+      onUnsubscribe: function(topic, container) {
+        log(container.getClientID() + " unsubscribes from tthis topic '" + topic + "'");
+        return true;
+      },
+      onPublish: function(topic, data, pcont, scont) {
+        log(pcont.getClientID() + " publishes '" + data + "' to topic '" + topic + "' subscribed by " + scont.getClientID());
+        return true;
+        // return false to reject the request.
+      }
+});
+	//  initialize managed hub for the Container
+	gadgets.pubsub2router.init(
+		    {
+		      hub: CommonContainer.managedHub
+		    });
+
+	 try {
+
+    // Connect to the ManagedHub
+    CommonContainer.inlineClient = new OpenAjax.hub.InlineContainer(CommonContainer.managedHub, "container",  
+        		 {
+                  Container: {
+                             onSecurityAlert:  function(source, alertType) { /* Handle client-side security alerts */  },
+                             onConnect:  function(container){        /* Called when client connects */   },
+                             onDisconnect:  function(container){        /* Called when client connects */   }
+                             }
+                   }
+                );
+    //connect to the inline client
+    CommonContainer.inlineClient.connect();
+   
+       
+     } catch(e) {
+         // TODO: error handling should be consistent with other OS gadget initialization error handling
+         alert("ERROR creating or connecting InlineClient in CommonContainer.managedHub [" + e.message + "]");
+     }
+	
+};
+
+//Wrapper function to set the gadget site/id and default width.  Currently have some inconsistency with width actually being set. This 
+//seems to be related to the pubsub2 feature.
+CommonContainer.renderGadget = function(gadgetURL, gadgetId) {
+	//going to hardcode these values for width.  
+    var el = document.getElementById("gadget-site-" + gadgetId);
+    var parms ={};
+    parms[shindig.container.RenderParam.WIDTH]="100%";
+	var gadgetSite = CommonContainer.newGadgetSite(el);
+	CommonContainer.navigateGadget(gadgetSite, gadgetURL, {}, parms);
+	return gadgetSite;
+  
+};
+//TODO:  To be implemented. Identify where to hook this into the page (in the gadget title bar/gadget management, etc)
+CommonContainer.navigateView = function(gadgetSite, gadgetURL, view) {
+	var renderParms ={};
+	if(view===null || view===""){
+		view="default";
+	}
+	//TODO Evaluate Parms based on configuration
+    renderParms[shindig.container.RenderParam.WIDTH]="100%";
+    renderParms['view']=view;
+
+    CommonContainer.navigateGadget(gadgetSite, gadgetURL, {}, renderParms);	
+};
+
+//TODO:  Add in UI controls in portlet header to remove gadget from the canvas
+CommonContainer.colapseGadget = function(gadgetSite) {
+	CommonContainer.closeGadget(gadgetSite);
+};
+
+//display the pubsub 2 event details
+function log(message) {
+  document.getElementById("output").innerHTML = gadgets.util.escapeString(message) + "<br/>" + document.getElementById("output").innerHTML;
+};
+
--- content/samplecontainer/examples/commoncontainer/layout.js
+++ content/samplecontainer/examples/commoncontainer/layout.js
@@ -34,7 +34,6 @@ $(function() {
 				.prepend('<span class="ui-icon ui-icon-minusthick"></span>')
 				.end()
 			.find(".portlet-content");
-
 		$(".portlet-header .ui-icon").click(function() {
 			$(this).toggleClass("ui-icon-minusthick").toggleClass("ui-icon-plusthick");
 			$(this).parents(".portlet:first").find(".portlet-content").toggle();
