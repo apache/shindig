ca24d4244d6392b73c605d994505a63dc4d3fa28
John Hjelmstad <johnh@apache.org>
2011-05-19 21:15:38 +0000

    Remove gadgets.config.[isGadget,isContainer] fields.
    git-svn-id: https://svn.apache.org/repos/asf/shindig/trunk@1125110 13f79535-47bb-0310-9956-ffa450edef68

--- features/src/main/javascript/features/osapi/gadgetsrpctransport.js
+++ features/src/main/javascript/features/osapi/gadgetsrpctransport.js
@@ -115,7 +115,7 @@ if (gadgets && gadgets.rpc) { //Dont bind if gadgets.rpc not defined
     }
 
     // Do not run this in container mode.
-    if (gadgets.config && gadgets.config.isGadget) {
+    if (gadgets.config && window !== window.top) {
       gadgets.config.register('osapi.services', null, init);
     }
   })();
