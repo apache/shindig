a1fb632d1e1744b6bb35acf2a1977984cb618190
Henry Saputra <hsaputra@apache.org>
2011-11-17 17:35:26 +0000

    Revert accidental checkins.
    git-svn-id: https://svn.apache.org/repos/asf/shindig/trunk@1203293 13f79535-47bb-0310-9956-ffa450edef68

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/spec/GadgetSpec.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/spec/GadgetSpec.java
@@ -107,13 +107,6 @@ public class GadgetSpec {
           viewElements.add(element);
         }
       }
-      if("ExternalServices".equals(name)) {
-        // There could be only one ExternalServices tag
-        if(externalServices != null) {
-          throw new SpecParserException("Only can have one ExternalServices is allowed.");
-        }
-        externalServices = new ExternalServices(element);
-      }
     }
 
     if (modulePrefs == null) {
@@ -207,14 +200,6 @@ public class GadgetSpec {
   protected Map<String, View> views;
   public Map<String, View> getViews() {
     return views;
-  }
-
-  /**
-   * ExternalServices
-   */
-  protected ExternalServices externalServices;
-  public ExternalServices getExternalServices() {
-    return externalServices;
   }
 
   /**
