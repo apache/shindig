154a4becf0ac529fa6cc2407b2f75ebd40f6e9b9
Ryan Baxter <rbaxter85@apache.org>
2012-05-02 16:51:59 +0000

    SHINDIG-1760 Add registerConextListener API for embedded experiences
    git-svn-id: https://svn.apache.org/repos/asf/shindig/trunk@1333106 13f79535-47bb-0310-9956-ffa450edef68

--- features/src/main/javascript/features/embeddedexperiences/embedded_experiences_gadgets.js
+++ features/src/main/javascript/features/embeddedexperiences/embedded_experiences_gadgets.js
@@ -17,12 +17,17 @@
  * under the License.
  */
 
-gadgets['embeddedExperiences'] = gadgets['embeddedExperiences'] || {};
+gadgets['ee'] = gadgets['ee'] || {};
 
 (function() {
+
+  var contextListeners = [];
+  var isContextSet = false;
+
   /**
    * Sets the context for this embedded experience.
-   * @param {Object} context the embedded experiences context.
+   * @param {Object} context
+   *         The embedded experiences context.
    */
   function setDataContext(context) {
     opensocial.data.DataContext.putDataSet('org.opensocial.ee.context', context);
@@ -31,15 +36,42 @@ gadgets['embeddedExperiences'] = gadgets['embeddedExperiences'] || {};
   /**
    * Init the embedded experiences feature.  This calls an RPC handler to get
    * the embedded experiences context and puts it in the gadgets data context.
-   * @param {Object} config configuration for the feature.
+   * @param {Object} config
+   *        Configuration for the feature.
    */
   function init(config) {
     gadgets.rpc.call(null, 'ee_gadget_rendered', setDataContext, {});
     gadgets.rpc.register('ee_set_context', setDataContext);
+    opensocial.data.getDataContext().registerListener('org.opensocial.ee.context', function(key) {
+      var context = opensocial.data.getDataContext().getDataSet(key);
+      isContextSet = true;
+      var length = contextListeners.length;
+      for(var i = length;  i--;) {
+        contextListeners[i](context);
+      }
+    });
   };
 
   if (gadgets.config) {
     gadgets.config.register('embedded-experiences', null, init);
   }
 
+  /**
+   * Registers a listener for when the embedded experiences context object is set for
+   * this gadget.  This listener will be called whenever the context is set for the gadget.
+   *
+   * @param {Function} listener
+   *        A function to be called when the listener is set.
+   */
+  gadgets.ee.registerContextListener = function(listener) {
+	//Add the listener regardless
+	contextListeners.push(listener);
+
+	//It could be that the context was already set before the gadget called this function
+	//so see if we have a context object in the data context and if we do call the listener
+	//back right away
+	if(isContextSet) {
+	  listener(opensocial.data.getDataContext().getDataSet('org.opensocial.ee.context'));
+	}
+  };
 }());
