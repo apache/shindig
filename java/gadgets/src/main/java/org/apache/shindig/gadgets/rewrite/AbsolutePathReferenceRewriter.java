417c260fba3817d7239e8e026c1b2596089434c3
Henry Saputra <hsaputra@apache.org>
2012-06-28 18:31:41 +0000

    Add optional shindig property shindig.gadgets.rewriter.absolutePath.tags to add possible tags that could have its path to be rewritten as absolute. Set default as RESOURCES.
    git-svn-id: https://svn.apache.org/repos/asf/shindig/trunk@1355096 13f79535-47bb-0310-9956-ffa450edef68

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/rewrite/AbsolutePathReferenceRewriter.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/rewrite/AbsolutePathReferenceRewriter.java
@@ -19,12 +19,16 @@
 package org.apache.shindig.gadgets.rewrite;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 
 import org.apache.shindig.common.uri.Uri;
 import org.apache.shindig.gadgets.Gadget;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Rewriter that converts all url's to absolute.
@@ -32,12 +36,37 @@ import java.util.List;
  * @since 2.0.0
  */
 public class AbsolutePathReferenceRewriter extends DomWalker.Rewriter {
+  private static final Logger LOG = Logger.getLogger(AbsolutePathReferenceRewriter.class.getName());
+
+  private AbsolutePathReferenceVisitor.Tags[] tags = {AbsolutePathReferenceVisitor.Tags.RESOURCES};
+
   @Inject
   public AbsolutePathReferenceRewriter() {}
 
+  @Inject(optional=true)
+  public void setAbsolutePathTags(@Named("shindig.gadgets.rewriter.absolutePath.tags")
+                                         String absolutePathTags) {
+    if(LOG.isLoggable(Level.FINE)) {
+      LOG.fine("Tags that should have the reference resolved to absolute path: " + absolutePathTags);
+    }
+    String[] tagsArray = absolutePathTags.split(",");
+    List<AbsolutePathReferenceVisitor.Tags> tagsList = Lists.newArrayList();
+    for(String tagValue : tagsArray) {
+      try {
+        AbsolutePathReferenceVisitor.Tags tag = AbsolutePathReferenceVisitor.Tags.valueOf(tagValue);
+        if(!tagsList.contains(tag)) {
+          tagsList.add(tag);
+        }
+      } catch (Exception ex) {
+        LOG.warning("Invalid absolute path tag name : " + tagValue);
+        continue;
+      }
+    }
+    this.tags = tagsList.toArray(new AbsolutePathReferenceVisitor.Tags[tagsList.size()]);
+  }
+
   @Override
   protected List<DomWalker.Visitor> makeVisitors(Gadget context, Uri gadgetUri) {
-    return ImmutableList.<DomWalker.Visitor>of(
-        new AbsolutePathReferenceVisitor(AbsolutePathReferenceVisitor.Tags.RESOURCES));
+    return ImmutableList.<DomWalker.Visitor>of(new AbsolutePathReferenceVisitor(tags));
   }
 }
