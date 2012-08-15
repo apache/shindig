c911502c893eae617f2a79d69745f14d278b36c4
Dan Dumont <ddumont@apache.org>
2012-08-15 14:41:08 +0000

    SHINDIG-1836 - upgrade OpenAjax Hub from 2.0.5 to 2.0.7 Committed for Marshall Shi
    git-svn-id: https://svn.apache.org/repos/asf/shindig/branches/Move OAH from 2.0.5 to 2.0.7@1373434 13f79535-47bb-0310-9956-ffa450edef68

--- extras/src/main/javascript/features-extras/org.openajax.hub-2.0.5/OpenAjax-mashup.js
+++ extras/src/main/javascript/features-extras/org.openajax.hub-2.0.5/OpenAjax-mashup.js
@@ -28,7 +28,7 @@ OpenAjax.hub = function() {
 
     return /** @scope OpenAjax.hub */ {
         implementer: "http://openajax.org",
-        implVersion: "2.0.5",
+        implVersion: "2.0.7",
         specVersion: "2.0",
         implExtraData: {},
         libraries: libs,
