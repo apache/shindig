c74e176e4d6b7b04ce6dbe14a18520bbcb4933be
Henry Saputra <hsaputra@apache.org>
2012-10-06 22:00:45 +0000

    Re-commit the fix to remove duplicate call to HttpClient.setHttpRequestRetryHandler
    git-svn-id: https://svn.apache.org/repos/asf/shindig/trunk@1395179 13f79535-47bb-0310-9956-ffa450edef68

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/http/BasicHttpFetcher.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/http/BasicHttpFetcher.java
@@ -220,7 +220,6 @@ public class BasicHttpFetcher implements HttpFetcher {
         }
       }
     });
-    client.setHttpRequestRetryHandler(new DefaultHttpRequestRetryHandler());
 
     // Disable automatic storage and sending of cookies (see SHINDIG-1382)
     client.removeRequestInterceptorByClass(RequestAddCookies.class);
